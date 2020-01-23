9a61af82ded21ac395bed54d5b4981c86908861c
geeksville <kevinh@geeksville.com>
2020-01-22 21:25:31 -0800

    add IPC mesh service for use by Signal

---
+++ app/src/main/aidl/com/geeksville/meshutil/IMeshService.aidl
@@ -0,0 +1,31 @@
+// com.geeksville.meshutil.IMeshService.aidl
+package com.geeksville.meshutil;
+
+// Declare any non-default types here with import statements
+
+interface IMeshService {
+    /**
+    * Set the ID info for this node
+    */
+    void setOwner(String myId, String longName, String shortName);
+
+    /*
+    Send an opaque packet to a specified node name
+    */
+    void sendOpaque(String destId, in byte[] payload);
+
+    /**
+    Get the IDs of everyone on the mesh.  You should also subscribe for NODE_CHANGE broadcasts.
+    */
+    void getOnline(out String[] ids);
+
+    /**
+    Is the packet radio currently connected to the phone?
+    */
+    boolean isConnected();
+
+    // see com.geeksville.meshutil broadcast intents
+    // RECEIVED_OPAQUE  for data received from other nodes
+    // NODE_CHANGE  for new IDs appearing or disappearing
+    // CONNECTION_CHANGED for losing/gaining connection to the packet radio
+}
---
+++ app/src/main/java/com/geeksville/meshutil/MeshService.kt
@@ -0,0 +1,31 @@
+package com.geeksville.meshutil
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class MeshService : Service() {
+
+    override fun onBind(intent: Intent): IBinder {
+        // Return the interface
+        return binder
+    }
+
+    private val binder = object : IMeshService.Stub() {
+        override fun setOwner(myId: String?, longName: String?, shortName: String?) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun sendOpaque(destId: String?, payload: ByteArray?) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun getOnline(ids: Array<out String>?) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun isConnected(): Boolean {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+    }
+}
