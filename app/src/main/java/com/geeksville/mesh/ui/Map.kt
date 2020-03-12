01f2d908a42128257d430f09fcfd133e6aa7727a
geeksville <kevinh@geeksville.com>
2020-03-11 18:13:44 -0700

    more map wip

--- app/src/main/java/com/geeksville/mesh/ui/Map.kt
+++ app/src/main/java/com/geeksville/mesh/ui/Map.kt
@@ -1,24 +1,21 @@
 package com.geeksville.mesh.ui
 
 import androidx.compose.Composable
+import androidx.ui.androidview.AndroidView
 import androidx.ui.core.ContextAmbient
-import androidx.ui.layout.Column
-import androidx.ui.layout.LayoutPadding
-import androidx.ui.layout.LayoutSize
 import androidx.ui.material.MaterialTheme
 import androidx.ui.tooling.preview.Preview
-import androidx.ui.unit.dp
+import com.geeksville.mesh.R
 
 
 @Composable
 fun MapContent() {
-    analyticsScreen(name = "channel")
+    analyticsScreen(name = "map")
 
     val typography = MaterialTheme.typography()
     val context = ContextAmbient.current
 
-    Column(modifier = LayoutSize.Fill + LayoutPadding(16.dp)) {
-    
+    AndroidView(R.layout.map_view) {
     }
 }
 
