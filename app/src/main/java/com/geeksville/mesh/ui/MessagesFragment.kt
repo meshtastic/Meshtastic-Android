5abb56ec3f24d2a2dd37941bfdef4568649f47b2
geeksville <kevinh@geeksville.com>
2020-04-21 20:48:44 -0700

    a better fix for the problem of sending messages while device is asleep
    Better just to Observe myId and wait for it to be !null

--- app/src/main/java/com/geeksville/mesh/ui/MessagesFragment.kt
+++ app/src/main/java/com/geeksville/mesh/ui/MessagesFragment.kt
@@ -169,10 +169,17 @@ class MessagesFragment : ScreenFragment("Messages"), Logging {
             messagesAdapter.onMessagesChanged(it)
         })
 
+        // If connection state _OR_ myID changes we have to fix our ability to edit outgoing messages
         model.isConnected.observe(viewLifecycleOwner, Observer { connected ->
             // If we don't know our node ID and we are offline don't let user try to send
             textInputLayout.isEnabled =
                 connected != MeshService.ConnectionState.DISCONNECTED && model.nodeDB.myId.value != null
+        })
+
+        model.nodeDB.myId.observe(viewLifecycleOwner, Observer { myId ->
+            // If we don't know our node ID and we are offline don't let user try to send
+            textInputLayout.isEnabled =
+                model.isConnected.value != MeshService.ConnectionState.DISCONNECTED && myId != null
         })
     }
 
