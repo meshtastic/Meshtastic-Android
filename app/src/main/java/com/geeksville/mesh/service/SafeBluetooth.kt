aae9690369c10dfb0a4a0f438ce29e90f597c21d
geeksville <kevinh@geeksville.com>
2020-06-18 15:47:36 -0700

    minor cleanups

--- app/src/main/java/com/geeksville/mesh/service/SafeBluetooth.kt
+++ app/src/main/java/com/geeksville/mesh/service/SafeBluetooth.kt
@@ -1,10 +1,7 @@
 package com.geeksville.mesh.service
 
 import android.bluetooth.*
-import android.content.BroadcastReceiver
 import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
 import android.os.Build
 import android.os.DeadObjectException
 import android.os.Handler
@@ -22,25 +19,8 @@ import java.util.*
 
 
 /// Return a standard BLE 128 bit UUID from the short 16 bit versions
-fun longBLEUUID(hexFour: String) = UUID.fromString("0000$hexFour-0000-1000-8000-00805f9b34fb")
+fun longBLEUUID(hexFour: String): UUID = UUID.fromString("0000$hexFour-0000-1000-8000-00805f9b34fb")
 
-
-/**
- * A helper class to call onChanged when bluetooth is enabled or disabled
- */
-class BluetoothStateReceiver(val onChanged: (Boolean) -> Unit) : BroadcastReceiver() {
-    val intent = IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED) // Can be used for registering
-
-    override fun onReceive(context: Context, intent: Intent) = exceptionReporter {
-        if (intent.action == BluetoothAdapter.ACTION_STATE_CHANGED) {
-            when (intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, -1)) {
-                // Simulate a disconnection if the user disables bluetooth entirely
-                BluetoothAdapter.STATE_OFF -> onChanged(false)
-                BluetoothAdapter.STATE_ON -> onChanged(true)
-            }
-        }
-    }
-}
 
 /**
  * Uses coroutines to safely access a bluetooth GATT device with a synchronous API
