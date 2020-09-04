0768ba086b1bb7fd574e3c7e3ca4fb1d343069f5
Ludovic Goix <ludovic.goix@gmail.com>
2020-09-04 07:59:07 -0400

    Show network range setting when locked #138

--- app/src/main/java/com/geeksville/mesh/model/ChannelOption.kt
+++ app/src/main/java/com/geeksville/mesh/model/ChannelOption.kt
@@ -7,5 +7,15 @@ enum class ChannelOption(val modemConfig: MeshProtos.ChannelSettings.ModemConfig
     SHORT(MeshProtos.ChannelSettings.ModemConfig.Bw125Cr45Sf128, R.string.modem_config_short),
     MEDIUM(MeshProtos.ChannelSettings.ModemConfig.Bw500Cr45Sf128, R.string.modem_config_medium),
     LONG(MeshProtos.ChannelSettings.ModemConfig.Bw31_25Cr48Sf512, R.string.modem_config_long),
-    VERY_LONG(MeshProtos.ChannelSettings.ModemConfig.Bw125Cr48Sf4096, R.string.modem_config_very_long)
+    VERY_LONG(MeshProtos.ChannelSettings.ModemConfig.Bw125Cr48Sf4096, R.string.modem_config_very_long);
+
+    companion object {
+        fun fromConfig(modemConfig: MeshProtos.ChannelSettings.ModemConfig?): ChannelOption? {
+            for (option in values()) {
+                if (option.modemConfig == modemConfig)
+                    return option
+            }
+            return null
+        }
+    }
 }
