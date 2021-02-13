a8d095d1d794390aedd00a7261a3a6527f4e0683
Vadim Furman <vfurman@gmail.com>
2021-02-12 17:40:47 -0800

    Fixed swapped short <-> medium channel names

--- app/src/main/java/com/geeksville/mesh/model/ChannelOption.kt
+++ app/src/main/java/com/geeksville/mesh/model/ChannelOption.kt
@@ -4,8 +4,8 @@ import com.geeksville.mesh.MeshProtos
 import com.geeksville.mesh.R
 
 enum class ChannelOption(val modemConfig: MeshProtos.ChannelSettings.ModemConfig, val configRes: Int) {
-    SHORT(MeshProtos.ChannelSettings.ModemConfig.Bw125Cr45Sf128, R.string.modem_config_short),
-    MEDIUM(MeshProtos.ChannelSettings.ModemConfig.Bw500Cr45Sf128, R.string.modem_config_medium),
+    SHORT(MeshProtos.ChannelSettings.ModemConfig.Bw500Cr45Sf128, R.string.modem_config_short),
+    MEDIUM(MeshProtos.ChannelSettings.ModemConfig.Bw125Cr45Sf128, R.string.modem_config_medium),
     LONG(MeshProtos.ChannelSettings.ModemConfig.Bw31_25Cr48Sf512, R.string.modem_config_long),
     VERY_LONG(MeshProtos.ChannelSettings.ModemConfig.Bw125Cr48Sf4096, R.string.modem_config_very_long);
 
