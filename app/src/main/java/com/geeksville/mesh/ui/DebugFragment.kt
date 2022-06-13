8e32ad81c06459089bed4c0fa2d888b096babcf7
andrekir <andrekir@pm.me>
2022-06-13 17:43:50 -0300

    fix DebugFragment ViewModel

--- app/src/main/java/com/geeksville/mesh/ui/DebugFragment.kt
+++ app/src/main/java/com/geeksville/mesh/ui/DebugFragment.kt
@@ -5,7 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
+import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.asLiveData
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -22,7 +22,7 @@ class DebugFragment : Fragment() {
     // This property is only valid between onCreateView and onDestroyView.
     private val binding get() = _binding!!
 
-    val model: UIViewModel by viewModels()
+    private val model: UIViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
