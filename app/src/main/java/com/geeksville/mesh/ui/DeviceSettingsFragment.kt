af287c2e6fd14030117d9283fb085a003371ae04
andrekir <andrekir@pm.me>
2023-03-02 21:59:22 -0300

    feat: update Compose in Fragments to use ComposeView

--- app/src/main/java/com/geeksville/mesh/ui/DeviceSettingsFragment.kt
+++ app/src/main/java/com/geeksville/mesh/ui/DeviceSettingsFragment.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.activityViewModels
+import com.geeksville.mesh.R
 import com.geeksville.mesh.android.Logging
-import com.geeksville.mesh.databinding.ComposeViewBinding
 import com.geeksville.mesh.model.UIViewModel
 import com.google.accompanist.themeadapter.material.MdcTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -15,27 +17,21 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class DeviceSettingsFragment : ScreenFragment("Device Settings"), Logging {
 
-    private var _binding: ComposeViewBinding? = null
-    private val binding get() = _binding!!
     private val model: UIViewModel by activityViewModels()
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = ComposeViewBinding.inflate(inflater, container, false)
-            .apply {
-                composeView.apply {
-                    setViewCompositionStrategy(
-                        ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed
-                    )
-                    setContent {
-                        MdcTheme {
-                            DeviceSettingsItemList(model)
-                        }
-                    }
+        return ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setBackgroundColor(ContextCompat.getColor(context, R.color.colorAdvancedBackground))
+            setContent {
+                MdcTheme {
+                    DeviceSettingsItemList(model)
                 }
             }
-        return binding.root
+        }
     }
 }
