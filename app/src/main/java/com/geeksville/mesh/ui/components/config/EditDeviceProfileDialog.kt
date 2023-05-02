b32006524c18c7f74bedb0ac9c0e6b781cf0bb2d
andrekir <andrekir@pm.me>
2023-05-02 06:52:01 -0300

    fix: set inputs to false when field is missing

--- app/src/main/java/com/geeksville/mesh/ui/components/config/EditDeviceProfileDialog.kt
+++ app/src/main/java/com/geeksville/mesh/ui/components/config/EditDeviceProfileDialog.kt
@@ -31,11 +31,11 @@ fun EditDeviceProfileDialog(
     onDismissRequest: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    var longNameInput by remember { mutableStateOf(true) }
-    var shortNameInput by remember { mutableStateOf(true) }
-    var channelUrlInput by remember { mutableStateOf(true) }
-    var configInput by remember { mutableStateOf(true) }
-    var moduleConfigInput by remember { mutableStateOf(true) }
+    var longNameInput by remember { mutableStateOf(deviceProfile.hasLongName()) }
+    var shortNameInput by remember { mutableStateOf(deviceProfile.hasShortName()) }
+    var channelUrlInput by remember { mutableStateOf(deviceProfile.hasChannelUrl()) }
+    var configInput by remember { mutableStateOf(deviceProfile.hasConfig()) }
+    var moduleConfigInput by remember { mutableStateOf(deviceProfile.hasModuleConfig()) }
 
     AlertDialog(
         title = { Text(title) },
