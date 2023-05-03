bac694e5d75539d79a6fce58a177b09b787e9883
PWRxPSYCHO <pwrxpsycho@gmail.com>
2023-05-03 15:56:18 -0400

    Added Cache Layout

--- app/src/main/java/com/geeksville/mesh/ui/map/components/CacheLayout.kt
+++ app/src/main/java/com/geeksville/mesh/ui/map/components/CacheLayout.kt
@@ -1,141 +1,149 @@
 package com.geeksville.mesh.ui.map.components
 
 import android.view.View
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.ContentAlpha
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import androidx.constraintlayout.widget.ConstraintLayout
 import com.geeksville.mesh.R
 
 @Composable
-fun CacheLayout() {
+fun CacheLayout(
+    onExecuteJob: () -> Unit,
+    onCancelDownload: () -> Unit
+) {
+    var selectedDistance by remember { mutableStateOf(5) }
 
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .padding(16.dp)
+//            .visibility(visibility = if (selectedDistance != -1) {
+//                View.VISIBLE
+//            } else {
+//                View.GONE
+//            })
+    ) {
+        Text(
+            text = stringResource(id = R.string.map_select_download_region),
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.h5,
+            color = MaterialTheme.colors.onBackground.copy(alpha = ContentAlpha.medium)
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        val distances = listOf(5, 10, 15)
+        val selectedDistanceIndex = distances.indexOf(selectedDistance)
+
+        ToggleButton(
+            options = distances.map { it.toString() },
+            selectedOptionIndex = selectedDistanceIndex,
+            onOptionSelected = { selectedDistance = distances[it].toInt() }
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(
+            text = stringResource(id = R.string.map_tile_download_estimate),
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.body1,
+            color = MaterialTheme.colors.onBackground.copy(alpha = ContentAlpha.medium)
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Button(
+                onClick = onExecuteJob,
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(end = 8.dp)
+            ) {
+                Text(
+                    text = stringResource(id = R.string.map_start_download),
+                    color = MaterialTheme.colors.onPrimary
+                )
+            }
+
+            Button(
+                onClick = onCancelDownload,
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(start = 8.dp)
+            ) {
+                Text(
+                    text = stringResource(id = R.string.cancel),
+                    color = MaterialTheme.colors.onPrimary
+                )
+            }
+        }
+    }
 }
-//    ConstraintLayout(
-//        modifier = Modifier
-//            .fillMaxWidth()
-//            .wrapContentHeight()
-//            .visibility(visibility = if (visible) View.VISIBLE else View.GONE)
-//    ) {
-//        val (title, toggleButton, cacheEstimate, executeJob, cancelDownload) = createRefs()
-//        Text(
-//            text = stringResource(id = R.string.map_select_download_region),
-//            fontSize = TextUnit.Sp(18),
-//            fontWeight = FontWeight.Bold,
-//            textAlign = TextAlign.Center,
-//            color = Color.Gray,
-//            modifier = Modifier
-//                .fillMaxWidth()
-//                .constrainAs(title) {
-//                    top.linkTo(parent.top)
-//                    start.linkTo(parent.start)
-//                    end.linkTo(parent.end)
-//                    bottom.linkTo(toggleButton.top)
-//                }
-//        )
-//
-//        val toggleButtonGroup = remember { mutableStateOf(1) }
-//
-//        ToggleButton(
-//            groupValue = toggleButtonGroup.value,
-//            onValueChange = { selectedValue -> toggleButtonGroup.value = selectedValue },
-//            modifier = Modifier
-//                .fillMaxWidth()
-//                .constrainAs(toggleButton) {
-//                    top.linkTo(title.bottom)
-//                    start.linkTo(parent.start)
-//                    end.linkTo(parent.end)
-//                    bottom.linkTo(cacheEstimate.top)
-//                }
-//        ) {
-//            ToggleButtonOption(
-//                id = 1,
-//                text = stringResource(id = R.string.map_5_miles)
-//            )
-//            ToggleButtonOption(
-//                id = 2,
-//                text = stringResource(id = R.string.map_10_miles)
-//            )
-//            ToggleButtonOption(
-//                id = 3,
-//                text = stringResource(id = R.string.map_15_miles)
-//            )
-//        }
-//
-//        Text(
-//            text = stringResource(id = R.string.map_tile_download_estimate),
-//            fontSize = TextUnit.Sp(14),
-//            color = Color.Gray,
-//            modifier = Modifier
-//                .constrainAs(cacheEstimate) {
-//                    top.linkTo(toggleButton.bottom)
-//                    start.linkTo(parent.start)
-//                    end.linkTo(parent.end)
-//                    bottom.linkTo(executeJob.top)
-//                }
-//        )
-//
-//        Button(
-//            onClick = { /* Perform action */ },
-//            modifier = Modifier
-//                .fillMaxWidth()
-//                .height(72.dp)
-//                .constrainAs(executeJob) {
-//                    top.linkTo(cacheEstimate.bottom)
-//                    start.linkTo(parent.start)
-//                    bottom.linkTo(parent.bottom)
-//                }
-//        ) {
-//            Text(
-//                text = stringResource(id = R.string.map_start_download),
-//                fontWeight = FontWeight.Bold
-//            )
-//        }
-//
-//        Button(
-//            onClick = { /* Perform action */ },
-//            modifier = Modifier
-//                .fillMaxWidth()
-//                .height(72.dp)
-//                .constrainAs(cancelDownload) {
-//                    top.linkTo(cacheEstimate.bottom)
-//                    end.linkTo(parent.end)
-//                    bottom.linkTo(parent.bottom)
-//                }
-//        ) {
-//            Text(text = stringResource(id = R.string.cancel))
-//        }
-//    }
-//}}
-//
-//@Composable
-//fun ToggleButton(
-//    groupValue: Int,
-//    onValueChange: (Int) -> Unit,
-//    modifier: Modifier = Modifier,
-//    content: @Composable () -> Unit
-//) {
-//    androidx.compose.material.ButtonGroup(
-//        selectedButton = groupValue - 1,
-//        modifier = modifier,
-//        options = {
-//            content()
-//        },
-//        onSelectedChange = { index -> onValueChange(index + 1) }
-//    )
-//}
-//
-//@Composable
-//fun ToggleButtonOption(id: Int, text: String) {
-//    androidx.compose.material.Button(
-//        onClick = {},
-//        colors = ButtonDefaults.buttonColors(
-//            backgroundColor = if (id == 1) Color.Gray else Color.White,
-//            contentColor = if
+
+@Composable
+fun ToggleButton(
+    options: List<String>,
+    selectedOptionIndex: Int,
+    onOptionSelected: (Int) -> Unit
+) {
+    val backgroundColor = MaterialTheme.colors.background
+    val selectedColor = MaterialTheme.colors.primary
+    val textColor = MaterialTheme.colors.onBackground.copy(alpha = ContentAlpha.medium)
+
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        options.forEachIndexed { index, option ->
+            val isSelected = index == selectedOptionIndex
+
+            Button(
+                onClick = { onOptionSelected(index) },
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = if (isSelected) selectedColor else backgroundColor,
+                    contentColor = textColor
+                ),
+                modifier = Modifier.weight(1f)
+            ) {
+                Text(
+                    text = option,
+                    textAlign = TextAlign.Center,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+            }
+
+            if (index != options.lastIndex) {
+                Spacer(modifier = Modifier.width(8.dp))
+            }
+        }
+    }
+}
+
