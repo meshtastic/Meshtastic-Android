3bbe3fd7f7a93d8acf6a9cf50d9c18e3e53a751b
andrekir <andrekir@pm.me>
2023-05-07 05:33:18 -0300

    refactor: simplify packetResponse using filterNotNull and firstOrNull

--- app/src/main/java/com/geeksville/mesh/model/UIState.kt
+++ app/src/main/java/com/geeksville/mesh/model/UIState.kt
@@ -40,6 +40,7 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.launchIn
@@ -156,10 +157,8 @@ class UIViewModel @Inject constructor(
             _ourNodeInfo.value = it
         }.launchIn(viewModelScope)
 
-        combine(_meshLog, requestId) { packet, requestId ->
-            requestId?.run { packet.lastOrNull { it.meshPacket?.decoded?.requestId == requestId } }
-        }.onEach {
-            _packetResponse.value = it
+        combine(meshLog, requestId.filterNotNull()) { packet, id ->
+            _packetResponse.value = packet.firstOrNull { it.meshPacket?.decoded?.requestId == id }
         }.launchIn(viewModelScope)
 
         debug("ViewModel created")
