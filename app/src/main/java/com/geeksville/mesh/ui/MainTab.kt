cf16f44a4886280e4dbdfe5480f00641bbcebffd
daviscodesbugs <daviscodesbugs@gmail.com>
2024-02-07 12:04:59 -0700

    Extract tab labels to string resources for easier translation Annotate resource IDs with their corresponding Android types

--- app/src/main/java/com/geeksville/mesh/ui/MainTab.kt
+++ app/src/main/java/com/geeksville/mesh/ui/MainTab.kt
@@ -1,36 +1,40 @@
 package com.geeksville.mesh.ui
 
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import androidx.fragment.app.Fragment
 import com.geeksville.mesh.R
 import com.geeksville.mesh.ui.map.MapFragment
 
 enum class MainTab(
-    val text: String,
+    @StringRes
+    val text: Int,
+    @DrawableRes
     val icon: Int,
     val content: Fragment
 ) {
     MESSAGES(
-        "Messages",
+        R.string.main_tab_lbl_messages,
         R.drawable.ic_twotone_message_24,
         ContactsFragment()
     ),
     USERS(
-        "Users",
+        R.string.main_tab_lbl_users,
         R.drawable.ic_twotone_people_24,
         UsersFragment()
     ),
     MAP(
-        "Map",
+        R.string.main_tab_lbl_map,
         R.drawable.ic_twotone_map_24,
         MapFragment()
     ),
     CHANNEL(
-        "Channel",
+        R.string.main_tab_lbl_channel,
         R.drawable.ic_twotone_contactless_24,
         ChannelFragment()
     ),
     SETTINGS(
-        "Settings",
+        R.string.main_tab_lbl_settings,
         R.drawable.ic_twotone_settings_applications_24,
         SettingsFragment()
     );
