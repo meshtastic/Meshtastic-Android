f36ff3dbfc9552f89bd607185a39c570f3875f2c
andrekir <andrekir@pm.me>
2024-02-15 08:48:11 -0300

    feat: add Singapore region info

--- app/src/main/java/com/geeksville/mesh/model/ChannelOption.kt
+++ app/src/main/java/com/geeksville/mesh/model/ChannelOption.kt
@@ -81,6 +81,7 @@ enum class RegionInfo(
     UA_868(RegionCode.UA_868, 868.0f, 868.6f),
     MY_433(RegionCode.MY_433, 433.0f, 435.0f),
     MY_919(RegionCode.MY_919, 919.0f, 924.0f),
+    SG_923(RegionCode.SG_923, 917.0f, 925.0f),
     LORA_24(RegionCode.LORA_24, 2400.0f, 2483.5f),
     UNSET(RegionCode.UNSET, 902.0f, 928.0f),
     ;
