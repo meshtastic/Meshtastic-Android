df032ee7750e8a6866da4e39d12ef0eb950c5543
daviscodesbugs <daviscodesbugs@gmail.com>
2024-02-29 16:53:25 -0700

    Dispose composition on recycle to avoid lingering spacing from previous layouts Remove comments explaning adapter functionality Remove unused methods

--- app/src/main/java/com/geeksville/mesh/ui/UsersFragment.kt
+++ app/src/main/java/com/geeksville/mesh/ui/UsersFragment.kt
@@ -4,8 +4,6 @@ import android.animation.ValueAnimator
 import android.content.res.ColorStateList
 import android.graphics.Color
 import android.os.Bundle
-import android.text.SpannableString
-import android.text.style.StrikethroughSpan
 import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
@@ -99,12 +97,6 @@ class UsersFragment : ScreenFragment("Users"), Logging {
         var nodes = arrayOf<NodeInfo>()
             private set
 
-        private fun CharSequence.strike() = SpannableString(this).apply {
-            setSpan(StrikethroughSpan(), 0, this.length, SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE)
-        }
-
-        private fun CharSequence.strikeIf(isIgnored: Boolean) = if (isIgnored) strike() else this
-
         private fun popup(view: View, position: Int) {
             if (!model.isConnected()) return
             val node = nodes[position]
@@ -173,61 +165,12 @@ class UsersFragment : ScreenFragment("Users"), Logging {
             popup.show()
         }
 
-        /**
-         * Called when RecyclerView needs a new [ViewHolder] of the given type to represent
-         * an item.
-         *
-         *
-         * This new ViewHolder should be constructed with a new View that can represent the items
-         * of the given type. You can either create a new View manually or inflate it from an XML
-         * layout file.
-         *
-         *
-         * The new ViewHolder will be used to display items of the adapter using
-         * [.onBindViewHolder]. Since it will be re-used to display
-         * different items in the data set, it is a good idea to cache references to sub views of
-         * the View to avoid unnecessary [View.findViewById] calls.
-         *
-         * @param parent The ViewGroup into which the new View will be added after it is bound to
-         * an adapter position.
-         * @param viewType The view type of the new View.
-         *
-         * @return A new ViewHolder that holds a View of the given view type.
-         * @see .getItemViewType
-         * @see .onBindViewHolder
-         */
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
             return ViewHolder(ComposeView(parent.context))
         }
 
-        /**
-         * Returns the total number of items in the data set held by the adapter.
-         *
-         * @return The total number of items in this adapter.
-         */
         override fun getItemCount(): Int = nodes.size
 
-        /**
-         * Called by RecyclerView to display the data at the specified position. This method should
-         * update the contents of the [ViewHolder.itemView] to reflect the item at the given
-         * position.
-         *
-         *
-         * Note that unlike [android.widget.ListView], RecyclerView will not call this method
-         * again if the position of the item changes in the data set unless the item itself is
-         * invalidated or the new position cannot be determined. For this reason, you should only
-         * use the `position` parameter while acquiring the related data item inside
-         * this method and should not keep a copy of it. If you need the position of an item later
-         * on (e.g. in a click listener), use [ViewHolder.getAdapterPosition] which will
-         * have the updated adapter position.
-         *
-         * Override [.onBindViewHolder] instead if Adapter can
-         * handle efficient partial bind.
-         *
-         * @param holder The ViewHolder which should be updated to represent the contents of the
-         * item at the given position in the data set.
-         * @param position The position of the item within the adapter's data set.
-         */
         override fun onBindViewHolder(holder: ViewHolder, position: Int) {
             val thisNode = nodes[0]
             val thatNode = nodes[position]
@@ -241,6 +184,10 @@ class UsersFragment : ScreenFragment("Users"), Logging {
             ) {
                 popup(holder.composeView, position)
             }
+        }
+
+        override fun onViewRecycled(holder: ViewHolder) {
+            holder.composeView.disposeComposition()
         }
 
         // Called when our node DB changes
