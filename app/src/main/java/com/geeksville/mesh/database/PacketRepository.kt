5b3c78316b8accc789471f7ebdcd36e295c76682
Andre K <andrekir@pm.me>
2024-06-08 10:25:47 -0300

    feat: support for switching between devices (#1078)

--- app/src/main/java/com/geeksville/mesh/database/PacketRepository.kt
+++ app/src/main/java/com/geeksville/mesh/database/PacketRepository.kt
@@ -2,6 +2,7 @@ package com.geeksville.mesh.database
 
 import com.geeksville.mesh.DataPacket
 import com.geeksville.mesh.MessageStatus
+import com.geeksville.mesh.Portnums.PortNum
 import com.geeksville.mesh.database.dao.PacketDao
 import com.geeksville.mesh.database.entity.ContactSettings
 import com.geeksville.mesh.database.entity.Packet
@@ -15,11 +16,13 @@ class PacketRepository @Inject constructor(private val packetDaoLazy: dagger.Laz
         packetDaoLazy.get()
     }
 
-    suspend fun getAllPackets(): Flow<List<Packet>> = withContext(Dispatchers.IO) {
-        packetDao.getAllPackets()
-    }
+    fun getWaypoints(): Flow<List<Packet>> = packetDao.getAllPackets(PortNum.WAYPOINT_APP_VALUE)
 
     fun getContacts(): Flow<Map<String, Packet>> = packetDao.getContactKeys()
+
+    suspend fun getMessageCount(contact: String): Int = withContext(Dispatchers.IO) {
+        packetDao.getMessageCount(contact)
+    }
 
     suspend fun getQueuedPackets(): List<DataPacket>? = withContext(Dispatchers.IO) {
         packetDao.getQueuedPackets()
@@ -29,9 +32,7 @@ class PacketRepository @Inject constructor(private val packetDaoLazy: dagger.Laz
         packetDao.insert(packet)
     }
 
-    suspend fun getMessagesFrom(contact: String) = withContext(Dispatchers.IO) {
-        packetDao.getMessagesFrom(contact)
-    }
+    fun getMessagesFrom(contact: String) = packetDao.getMessagesFrom(contact)
 
     suspend fun updateMessageStatus(d: DataPacket, m: MessageStatus) = withContext(Dispatchers.IO) {
         packetDao.updateMessageStatus(d, m)
@@ -45,14 +46,14 @@ class PacketRepository @Inject constructor(private val packetDaoLazy: dagger.Laz
         packetDao.getDataPacketById(requestId)
     }
 
-    suspend fun deleteAllMessages() = withContext(Dispatchers.IO) {
-        packetDao.deleteAllMessages()
-    }
-
     suspend fun deleteMessages(uuidList: List<Long>) = withContext(Dispatchers.IO) {
         for (chunk in uuidList.chunked(500)) { // limit number of UUIDs per query
             packetDao.deleteMessages(chunk)
         }
+    }
+
+    suspend fun deleteContacts(contactList: List<String>) = withContext(Dispatchers.IO) {
+        packetDao.deleteContacts(contactList)
     }
 
     suspend fun deleteWaypoint(id: Int) = withContext(Dispatchers.IO) {
