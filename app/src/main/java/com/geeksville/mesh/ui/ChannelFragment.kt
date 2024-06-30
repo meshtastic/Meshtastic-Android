c571359a0a3a6426af10cbe1740f7f0400404780
andrekir <andrekir@pm.me>
2024-06-30 10:17:00 -0300

    fix: missing import

--- app/src/main/java/com/geeksville/mesh/ui/ChannelFragment.kt
+++ app/src/main/java/com/geeksville/mesh/ui/ChannelFragment.kt
@@ -17,6 +17,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
