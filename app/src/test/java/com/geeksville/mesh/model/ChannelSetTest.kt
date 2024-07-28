ae44c7a5f7321309236dd8b9a3f76c2b4efa7eb1
AddisonTustin <addisontustin@gmail.com>
2024-07-28 04:50:54 -0700

    feat: support add=true in QR codes (#1151)

--- app/src/test/java/com/geeksville/mesh/model/ChannelSetTest.kt
+++ app/src/test/java/com/geeksville/mesh/model/ChannelSetTest.kt
@@ -5,6 +5,7 @@ import org.junit.Assert
 import org.junit.Test
 
 class ChannelSetTest {
+
     /** make sure we match the python and device code behavior */
     @Test
     fun matchPython() {
@@ -12,5 +13,35 @@ class ChannelSetTest {
         val cs = url.toChannelSet()
         Assert.assertEquals("LongFast", cs.primaryChannel!!.name)
         Assert.assertEquals(url, cs.getChannelUrl(false))
+    }
+
+    /** validate against the host or path in a case-insensitive way */
+    @Test
+    fun parseCaseInsensitive() {
+        var url = Uri.parse("HTTPS://MESHTASTIC.ORG/E/#CgMSAQESBggBQANIAQ")
+        Assert.assertEquals("LongFast", url.toChannelSet().primaryChannel!!.name)
+
+        url = Uri.parse("HTTPS://mEsHtAsTiC.OrG/e/#CgMSAQESBggBQANIAQ")
+        Assert.assertEquals("LongFast", url.toChannelSet().primaryChannel!!.name)
+    }
+
+    /** properly parse channel config when `?add=true` is in the fragment */
+    @Test
+    fun handleAddInFragment() {
+        val url = Uri.parse("https://meshtastic.org/e/#CgMSAQESBggBQANIAQ?add=true")
+        val cs = url.toChannelSet()
+        val shouldAdd = url.shouldAddChannels()
+        Assert.assertEquals("LongFast", cs.primaryChannel!!.name)
+        Assert.assertTrue(shouldAdd)
+    }
+
+    /** properly parse channel config when `?add=true` is in the query parameters */
+    @Test
+    fun handleAddInQueryParams() {
+        val url = Uri.parse("https://meshtastic.org/e/?add=true#CgMSAQESBggBQANIAQ")
+        val cs = url.toChannelSet()
+        val shouldAdd = url.shouldAddChannels()
+        Assert.assertEquals("LongFast", cs.primaryChannel!!.name)
+        Assert.assertTrue(shouldAdd)
     }
 }
