61101981c883c65b9b73f20e9a133f70edbe8189
James Rich <james.rich@shiftdsm.com>
2024-08-03 10:43:28 -0500

    Fix: hoist `LaunchedEffect` to screen level to provide `currentTimeMillis` every minute for recalculation and refreshing the view.
    Also refactors `formatAgo` function for readability.

--- app/src/main/java/com/geeksville/mesh/ui/LastHeardInfo.kt
+++ app/src/main/java/com/geeksville/mesh/ui/LastHeardInfo.kt
@@ -7,47 +7,25 @@ import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.compose.LifecycleResumeEffect
 import com.geeksville.mesh.R
 import com.geeksville.mesh.ui.theme.AppTheme
 import com.geeksville.mesh.util.formatAgo
-import kotlinx.coroutines.delay
-import kotlin.time.Duration.Companion.minutes
 
 @Composable
 fun LastHeardInfo(
     modifier: Modifier = Modifier,
-    lastHeard: Int
+    lastHeard: Int,
+    currentTimeMillis: Long,
 ) {
-    var ago by remember { mutableStateOf(formatAgo(lastHeard)) }
-
-    var running by remember { mutableStateOf(false) }
-    LaunchedEffect(running) {
-        if (running) {
-            while (true) {
-                delay(1.minutes)
-                ago = formatAgo(lastHeard)
-            }
-        }
-    }
-
-    LifecycleResumeEffect(Unit) {
-        running = true
-        onPauseOrDispose {
-            running = false
-        }
-    }
 
     Row(
         modifier = modifier,
@@ -61,7 +39,7 @@ fun LastHeardInfo(
             tint = MaterialTheme.colors.onSurface,
         )
         Text(
-            text = ago,
+            text = formatAgo(lastHeard, currentTimeMillis),
             color = MaterialTheme.colors.onSurface,
             fontSize = MaterialTheme.typography.button.fontSize
         )
@@ -72,6 +50,9 @@ fun LastHeardInfo(
 @Composable
 fun LastHeardInfoPreview() {
     AppTheme {
-        LastHeardInfo(lastHeard = (System.currentTimeMillis() / 1000).toInt() - 8600)
+        LastHeardInfo(
+            lastHeard = (System.currentTimeMillis() / 1000).toInt() - 8600,
+            currentTimeMillis = System.currentTimeMillis()
+        )
     }
 }
