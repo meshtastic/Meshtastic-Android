d438872740e40aad8aae61eb383f901ef0b12568
James Rich <james.rich@shiftdsm.com>
2024-08-03 08:00:15 -0500

    Fix: Periodically update last heard time
    This commit introduces a periodic update to the last heard time displayed in the UI. The time is now refreshed every 1 minute
     to ensure accurate and up-to-date information.

--- app/src/main/java/com/geeksville/mesh/ui/LastHeardInfo.kt
+++ app/src/main/java/com/geeksville/mesh/ui/LastHeardInfo.kt
@@ -7,21 +7,48 @@ import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.LifecycleResumeEffect
 import com.geeksville.mesh.R
 import com.geeksville.mesh.ui.theme.AppTheme
 import com.geeksville.mesh.util.formatAgo
+import kotlinx.coroutines.delay
+import kotlin.time.Duration.Companion.minutes
 
 @Composable
 fun LastHeardInfo(
     modifier: Modifier = Modifier,
     lastHeard: Int
 ) {
+    var ago by remember { mutableStateOf(formatAgo(lastHeard)) }
+
+    var running by remember { mutableStateOf(false) }
+    LaunchedEffect(running) {
+        if (running) {
+            while (true) {
+                delay(1.minutes)
+                ago = formatAgo(lastHeard)
+            }
+        }
+    }
+
+    LifecycleResumeEffect(Unit) {
+        running = true
+        onPauseOrDispose {
+            running = false
+        }
+    }
+
     Row(
         modifier = modifier,
         verticalAlignment = Alignment.CenterVertically,
@@ -34,7 +61,7 @@ fun LastHeardInfo(
             tint = MaterialTheme.colors.onSurface,
         )
         Text(
-            text = formatAgo(lastHeard),
+            text = ago,
             color = MaterialTheme.colors.onSurface,
             fontSize = MaterialTheme.typography.button.fontSize
         )
