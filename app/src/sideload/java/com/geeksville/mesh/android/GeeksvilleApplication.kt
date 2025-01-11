dad0997f1db495add0980955db125d337c561a5e
James Rich <2199651+jamesarich@users.noreply.github.com>
2025-01-11 17:25:36 -0600

    feat: Add sideload build flavor
    This commit adds a new "sideload" build flavor based off the fdroid flavor, along with its related resources and application class.

---
+++ app/src/sideload/java/com/geeksville/mesh/android/GeeksvilleApplication.kt
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2025 Meshtastic LLC
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.geeksville.mesh.android
+
+import android.app.Application
+import android.content.Context
+import android.content.SharedPreferences
+import android.provider.Settings
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.edit
+import com.geeksville.mesh.analytics.AnalyticsProvider
+
+open class GeeksvilleApplication : Application(), Logging {
+
+    companion object {
+        lateinit var analytics: AnalyticsProvider
+    }
+
+    /// Are we running inside the testlab?
+    val isInTestLab: Boolean
+        get() {
+            val testLabSetting =
+                Settings.System.getString(contentResolver, "firebase.test.lab") ?: null
+            if(testLabSetting != null)
+                info("Testlab is $testLabSetting")
+            return "true" == testLabSetting
+        }
+
+    private val analyticsPrefs: SharedPreferences by lazy {
+        getSharedPreferences("analytics-prefs", Context.MODE_PRIVATE)
+    }
+
+    var isAnalyticsAllowed: Boolean
+        get() = analyticsPrefs.getBoolean("allowed", true)
+        set(value) {
+            analyticsPrefs.edit {
+                putBoolean("allowed", value)
+            }
+
+            // Change the flag with the providers
+            analytics.setEnabled(value && !isInTestLab) // Never do analytics in the test lab
+        }
+
+    @Suppress("UNUSED_PARAMETER")
+    fun askToRate(activity: AppCompatActivity) {
+        // do nothing
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        val nopAnalytics = com.geeksville.mesh.analytics.NopAnalytics(this)
+        analytics = nopAnalytics
+        isAnalyticsAllowed = false
+    }
+}
+
+fun Context.isGooglePlayAvailable(): Boolean = false
