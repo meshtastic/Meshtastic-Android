002e4a31c00ea835bfd9f33b69e2c1fa0c5ec109
Joshua Soberg <soberg.dev@pm.me>
2025-02-22 11:27:27 -0500

    Add large font preview

---
+++ app/src/main/java/com/geeksville/mesh/ui/compose/preview/LargeFontPreview.kt
@@ -0,0 +1,6 @@
+package com.geeksville.mesh.ui.compose.preview
+
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(name = "Large Font", fontScale = 2f)
+annotation class LargeFontPreview
