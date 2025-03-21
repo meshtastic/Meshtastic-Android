9e03611ea5a219d9f52ebf4323a344bcff00c6fb
niccellular <79813408+niccellular@users.noreply.github.com>
2025-03-21 17:43:34 -0400

    Update MeshService.kt

--- app/src/main/java/com/geeksville/mesh/service/MeshService.kt
+++ app/src/main/java/com/geeksville/mesh/service/MeshService.kt
@@ -663,7 +663,7 @@ class MeshService : Service(), Logging {
     private fun toMeshPacket(p: DataPacket): MeshPacket {
         return newMeshPacketTo(p.to!!).buildMeshPacket(
             id = p.id,
-            wantAck = p.wantAck,
+            wantAck = (Boolean) p.wantAck,
             hopLimit = p.hopLimit,
             channel = p.channel,
         ) {
