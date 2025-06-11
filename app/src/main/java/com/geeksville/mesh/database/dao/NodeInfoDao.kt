f8677731a7c7e7163cd3f4d759fd9e1a713d9263
Łukasz Kosson <git@lukasz.kosson.net>
2025-06-11 09:52:29 +0200

    fixed negative node number formatting

--- app/src/main/java/com/geeksville/mesh/database/dao/NodeInfoDao.kt
+++ app/src/main/java/com/geeksville/mesh/database/dao/NodeInfoDao.kt
@@ -70,8 +70,8 @@ interface NodeInfoDao {
         AND (:filter = ''
             OR (long_name LIKE '%' || :filter || '%'
             OR short_name LIKE '%' || :filter || '%'
-            OR '!' || substr(printf('%08x', num), -8) LIKE '%' || :filter || '%'
-            OR printf('%i', num) LIKE '%' || :filter || '%'))
+            OR printf('!%08x', CASE WHEN num < 0 THEN num + 4294967296 ELSE num END) LIKE '%' || :filter || '%'
+            OR CASE WHEN num < 0 THEN num + 4294967296 ELSE num END LIKE '%' || :filter || '%'))
         AND (:lastHeardMin = -1 OR last_heard >= :lastHeardMin)
         AND (:hopsAwayMax = -1 OR (hops_away <= :hopsAwayMax AND hops_away >= 0) OR num = (SELECT myNodeNum FROM my_node LIMIT 1))
     ORDER BY CASE
