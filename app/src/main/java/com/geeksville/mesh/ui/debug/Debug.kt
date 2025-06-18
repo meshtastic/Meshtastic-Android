bc5ac41cdb95809bd63ba42ca38d7659dcec068f
Dane Evans <dane@goneepic.com>
2025-06-18 13:12:19 +1000

    fix suppression

--- app/src/main/java/com/geeksville/mesh/ui/debug/Debug.kt
+++ app/src/main/java/com/geeksville/mesh/ui/debug/Debug.kt
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-// @Suppress("detekt:TooManyFunctions") // Lots of necessary previews
+@file:Suppress("detekt:TooManyFunctions") // Lots of necessary previews
 
 
 package com.geeksville.mesh.ui.debug
