ea4220e07c54349a8f54cb93cad537494fd436ce
James Rich <2199651+jamesarich@users.noreply.github.com>
2025-07-08 14:42:50 -0500

    Refactor: Use `isNotEmpty()` for PKC check in NodeEntity
    This commit updates the `hasPKC` property in `NodeEntity.kt` to use the `isNotEmpty()` extension function for checking the presence of a public key.
    
    This change leverages the Kotlin standard library for a more idiomatic check, considering either the `publicKey` property or the `user.publicKey` if the former is null.
    
    Signed-off-by: James Rich <2199651+jamesarich@users.noreply.github.com>

--- app/src/main/java/com/geeksville/mesh/database/entity/NodeEntity.kt
+++ app/src/main/java/com/geeksville/mesh/database/entity/NodeEntity.kt
@@ -35,6 +35,7 @@ import com.geeksville.mesh.copy
 import com.geeksville.mesh.model.Node
 import com.geeksville.mesh.util.onlineTimeThreshold
 import com.google.protobuf.ByteString
+import com.google.protobuf.kotlin.isNotEmpty
 
 data class NodeWithRelations(
     @Embedded val node: NodeEntity,
@@ -154,7 +155,7 @@ data class NodeEntity(
         get() = environmentTelemetry.environmentMetrics
 
     val isUnknownUser get() = user.hwModel == MeshProtos.HardwareModel.UNSET
-    val hasPKC get() = !user.publicKey.isEmpty
+    val hasPKC get() = (publicKey ?: user.publicKey).isNotEmpty()
 
     fun setPosition(p: MeshProtos.Position, defaultTime: Int = currentTime()) {
         position = p.copy { time = if (p.time != 0) p.time else defaultTime }
