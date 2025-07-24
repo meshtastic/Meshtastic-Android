ba99c1484033e359149c02bee908bb9ce4e2fd09
James Rich <2199651+jamesarich@users.noreply.github.com>
2025-07-24 17:09:28 -0500

    refactor(colors): consolidate colors in UI components
    Adds `CustomColors.kt` which consolidates the various custom colors used throughout the application.
    
    Aligns colors for visual consistency.
    
    Signed-off-by: James Rich <2199651+jamesarich@users.noreply.github.com>

--- app/src/main/java/com/geeksville/mesh/ui/node/components/NodeKeyStatusIcon.kt
+++ app/src/main/java/com/geeksville/mesh/ui/node/components/NodeKeyStatusIcon.kt
@@ -36,6 +36,7 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -46,7 +47,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
@@ -58,72 +58,54 @@ import com.geeksville.mesh.R
 import com.geeksville.mesh.model.Channel
 import com.geeksville.mesh.ui.common.components.CopyIconButton
 import com.geeksville.mesh.ui.common.theme.AppTheme
+import com.geeksville.mesh.ui.common.theme.StatusColors.StatusGreen
+import com.geeksville.mesh.ui.common.theme.StatusColors.StatusRed
+import com.geeksville.mesh.ui.common.theme.StatusColors.StatusYellow
 import com.google.protobuf.ByteString
 
 @Composable
-private fun KeyStatusDialog(
-    @StringRes title: Int,
-    @StringRes text: Int,
-    key: ByteString?,
-    onDismiss: () -> Unit = {}
-) = Dialog(
-    onDismissRequest = onDismiss,
-) {
-    Surface(
-        modifier = Modifier.fillMaxWidth(),
-        shape = RoundedCornerShape(16.dp),
-        color = MaterialTheme.colorScheme.background
-    ) {
-        LazyColumn(
-            contentPadding = PaddingValues(horizontal = 24.dp, vertical = 16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
+private fun KeyStatusDialog(@StringRes title: Int, @StringRes text: Int, key: ByteString?, onDismiss: () -> Unit = {}) =
+    Dialog(onDismissRequest = onDismiss) {
+        Surface(
+            modifier = Modifier.fillMaxWidth(),
+            shape = RoundedCornerShape(16.dp),
+            color = MaterialTheme.colorScheme.background,
         ) {
-            item {
-                Text(
-                    text = stringResource(id = title),
-                    textAlign = TextAlign.Center,
-                )
-                Spacer(Modifier.height(16.dp))
-                Text(
-                    text = stringResource(id = text),
-                    textAlign = TextAlign.Center,
-                )
-                Spacer(Modifier.height(16.dp))
-                if (key != null && title == R.string.encryption_pkc) {
-                    val keyString = Base64.encodeToString(key.toByteArray(), Base64.NO_WRAP)
-                    Text(
-                        text = stringResource(id = R.string.config_security_public_key) + ":",
-                        textAlign = TextAlign.Center,
-                    )
-                    Spacer(Modifier.height(8.dp))
-                    SelectionContainer {
+            LazyColumn(
+                contentPadding = PaddingValues(horizontal = 24.dp, vertical = 16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                item {
+                    Text(text = stringResource(id = title), textAlign = TextAlign.Center)
+                    Spacer(Modifier.height(16.dp))
+                    Text(text = stringResource(id = text), textAlign = TextAlign.Center)
+                    Spacer(Modifier.height(16.dp))
+                    if (key != null && title == R.string.encryption_pkc) {
+                        val keyString = Base64.encodeToString(key.toByteArray(), Base64.NO_WRAP)
                         Text(
-                            text = keyString,
+                            text = stringResource(id = R.string.config_security_public_key) + ":",
                             textAlign = TextAlign.Center,
                         )
+                        Spacer(Modifier.height(8.dp))
+                        SelectionContainer { Text(text = keyString, textAlign = TextAlign.Center) }
+                        Spacer(Modifier.height(8.dp))
+                        CopyIconButton(valueToCopy = keyString, modifier = Modifier.padding(start = 8.dp))
+                        Spacer(Modifier.height(16.dp))
                     }
-                    Spacer(Modifier.height(8.dp))
-                    CopyIconButton(
-                        valueToCopy = keyString,
-                        modifier = Modifier.padding(start = 8.dp)
-                    )
-                    Spacer(Modifier.height(16.dp))
-                }
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.End,
-                ) {
-                    TextButton(
-                        onClick = onDismiss,
-                        colors = ButtonDefaults.textButtonColors(
-                            contentColor = MaterialTheme.colorScheme.onSurface,
-                        ),
-                    ) { Text(text = stringResource(id = R.string.close)) }
+                    Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.End) {
+                        TextButton(
+                            onClick = onDismiss,
+                            colors = ButtonDefaults.textButtonColors(
+                                contentColor = MaterialTheme.colorScheme.onSurface,
+                            ),
+                        ) {
+                            Text(text = stringResource(id = R.string.close))
+                        }
+                    }
                 }
             }
         }
     }
-}
 
 @Composable
 fun NodeKeyStatusIcon(
@@ -134,32 +116,33 @@ fun NodeKeyStatusIcon(
 ) {
     var showEncryptionDialog by remember { mutableStateOf(false) }
     if (showEncryptionDialog) {
-        val (title, text) = when {
-            mismatchKey -> R.string.encryption_error to R.string.encryption_error_text
-            hasPKC -> R.string.encryption_pkc to R.string.encryption_pkc_text
-            else -> R.string.encryption_psk to R.string.encryption_psk_text
-        }
+        val (title, text) =
+            when {
+                mismatchKey -> R.string.encryption_error to R.string.encryption_error_text
+                hasPKC -> R.string.encryption_pkc to R.string.encryption_pkc_text
+                else -> R.string.encryption_psk to R.string.encryption_psk_text
+            }
         KeyStatusDialog(title, text, publicKey) { showEncryptionDialog = false }
     }
 
-    val (icon, tint) = when {
-        mismatchKey -> Icons.Default.KeyOff to Color.Red
-        hasPKC -> Icons.Default.Lock to Color(color = 0xFF30C047)
-        else -> ImageVector.vectorResource(R.drawable.ic_lock_open_right_24) to Color(color = 0xFFFEC30A)
-    }
+    val (icon, tint) =
+        when {
+            mismatchKey -> Icons.Default.KeyOff to colorScheme.StatusRed
+            hasPKC -> Icons.Default.Lock to colorScheme.StatusGreen
+            else -> ImageVector.vectorResource(R.drawable.ic_lock_open_right_24) to colorScheme.StatusYellow
+        }
 
-    IconButton(
-        onClick = { showEncryptionDialog = true },
-        modifier = modifier,
-    ) {
+    IconButton(onClick = { showEncryptionDialog = true }, modifier = modifier) {
         Icon(
             imageVector = icon,
-            contentDescription = stringResource(
-                id = when {
+            contentDescription =
+            stringResource(
+                id =
+                when {
                     mismatchKey -> R.string.encryption_error
                     hasPKC -> R.string.encryption_pkc
                     else -> R.string.encryption_psk
-                }
+                },
             ),
             tint = tint,
         )
@@ -169,13 +152,7 @@ fun NodeKeyStatusIcon(
 @PreviewLightDark
 @Composable
 private fun KeyStatusDialogErrorPreview() {
-    AppTheme {
-        KeyStatusDialog(
-            title = R.string.encryption_error,
-            text = R.string.encryption_error_text,
-            key = null,
-        )
-    }
+    AppTheme { KeyStatusDialog(title = R.string.encryption_error, text = R.string.encryption_error_text, key = null) }
 }
 
 @PreviewLightDark
@@ -193,11 +170,5 @@ private fun KeyStatusDialogPkcPreview() {
 @PreviewLightDark
 @Composable
 private fun KeyStatusDialogPskPreview() {
-    AppTheme {
-        KeyStatusDialog(
-            title = R.string.encryption_psk,
-            text = R.string.encryption_psk_text,
-            key = null,
-        )
-    }
+    AppTheme { KeyStatusDialog(title = R.string.encryption_psk, text = R.string.encryption_psk_text, key = null) }
 }
