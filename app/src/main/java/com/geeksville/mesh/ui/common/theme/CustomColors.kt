6e2aabc9d19267785c4f16c3811c894a04c6e6f0
James Rich <2199651+jamesarich@users.noreply.github.com>
2025-07-24 18:59:50 -0500

    finish extracting graph colors
    Signed-off-by: James Rich <2199651+jamesarich@users.noreply.github.com>

--- app/src/main/java/com/geeksville/mesh/ui/common/theme/CustomColors.kt
+++ app/src/main/java/com/geeksville/mesh/ui/common/theme/CustomColors.kt
@@ -41,10 +41,14 @@ object GraphColors {
     val InfantryBlue = Color(red = 75, green = 119, blue = 190)
     val Purple = Color(0xFF9C27B0)
     val Pink = Color(red = 255, green = 102, blue = 204)
-    val Orange = Color(0xFFFF8800)
+    val Orange = Color
 
     val Green = Color.Green
     val Red = Color.Red
+    val Blue = Color.Blue
+    val Yellow = Color.Yellow
+    val Magenta = Color.Magenta
+    val Cyan = Color.Cyan
 }
 
 object StatusColors {
