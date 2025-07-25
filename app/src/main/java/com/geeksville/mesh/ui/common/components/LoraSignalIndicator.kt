6fd444c0778637506e01d741ce34e972705a0048
James Rich <2199651+jamesarich@users.noreply.github.com>
2025-07-24 21:27:33 -0500

    refactor(colors): consolidate colors in UI components (#2520)
    Signed-off-by: James Rich <2199651+jamesarich@users.noreply.github.com>

--- app/src/main/java/com/geeksville/mesh/ui/common/components/LoraSignalIndicator.kt
+++ app/src/main/java/com/geeksville/mesh/ui/common/components/LoraSignalIndicator.kt
@@ -36,16 +36,22 @@ import androidx.compose.material.icons.filled.SignalCellularAlt1Bar
 import androidx.compose.material.icons.filled.SignalCellularAlt2Bar
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
 import com.geeksville.mesh.R
+import com.geeksville.mesh.ui.common.theme.StatusColors.StatusGreen
+import com.geeksville.mesh.ui.common.theme.StatusColors.StatusOrange
+import com.geeksville.mesh.ui.common.theme.StatusColors.StatusRed
+import com.geeksville.mesh.ui.common.theme.StatusColors.StatusYellow
 
 private const val SNR_GOOD_THRESHOLD = -7f
 private const val SNR_FAIR_THRESHOLD = -15f
@@ -53,29 +59,27 @@ private const val SNR_FAIR_THRESHOLD = -15f
 private const val RSSI_GOOD_THRESHOLD = -115
 private const val RSSI_FAIR_THRESHOLD = -126
 
+@Stable
 private enum class Quality(
-    val nameRes: Int,
-    val imageVector: ImageVector,
-    val color: Color
+    @Stable val nameRes: Int,
+    @Stable val imageVector: ImageVector,
+    @Stable val color: @Composable () -> Color,
 ) {
-    NONE(R.string.none_quality, Icons.Default.SignalCellularAlt1Bar, Color.Red),
-    BAD(R.string.bad, Icons.Default.SignalCellularAlt2Bar, Color(red = 247, green = 147, blue = 26)),
-    FAIR(R.string.fair, Icons.Default.SignalCellularAlt, Color(red = 255, green = 230, blue = 0)),
-    GOOD(R.string.good, Icons.Default.SignalCellular4Bar, Color(0xFF30C047))
+    NONE(R.string.none_quality, Icons.Default.SignalCellularAlt1Bar, { colorScheme.StatusRed }),
+    BAD(R.string.bad, Icons.Default.SignalCellularAlt2Bar, { colorScheme.StatusOrange }),
+    FAIR(R.string.fair, Icons.Default.SignalCellularAlt, { colorScheme.StatusYellow }),
+    GOOD(R.string.good, Icons.Default.SignalCellular4Bar, { colorScheme.StatusGreen }),
 }
 
 /**
- * Displays the `snr` and `rssi` color coded based on the signal quality, along with
- * a human readable description and related icon.
+ * Displays the `snr` and `rssi` color coded based on the signal quality, along with a human readable description and
+ * related icon.
  */
 @OptIn(ExperimentalLayoutApi::class)
 @Composable
 fun NodeSignalQuality(snr: Float, rssi: Int, modifier: Modifier = Modifier) {
     val quality = determineSignalQuality(snr, rssi)
-    FlowRow(
-        modifier = modifier,
-        maxLines = 1,
-    ) {
+    FlowRow(modifier = modifier, maxLines = 1) {
         Snr(snr)
         Spacer(Modifier.width(8.dp))
         Rssi(rssi)
@@ -89,44 +93,34 @@ fun NodeSignalQuality(snr: Float, rssi: Int, modifier: Modifier = Modifier) {
         Icon(
             imageVector = quality.imageVector,
             contentDescription = stringResource(R.string.signal_quality),
-            tint = quality.color
+            tint = quality.color.invoke(),
         )
     }
 }
 
-/**
- * Displays the `snr` and `rssi` with color depending on the values respectively.
- */
+/** Displays the `snr` and `rssi` with color depending on the values respectively. */
 @Composable
 fun SnrAndRssi(snr: Float, rssi: Int) {
-    Row(
-        modifier = Modifier.fillMaxWidth(),
-        horizontalArrangement = Arrangement.SpaceBetween
-    ) {
+    Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
         Snr(snr)
         Rssi(rssi)
     }
 }
 
-/**
- * Displays a human readable description and icon representing the signal quality.
- */
+/** Displays a human readable description and icon representing the signal quality. */
 @Composable
 fun LoraSignalIndicator(snr: Float, rssi: Int) {
-
     val quality = determineSignalQuality(snr, rssi)
 
     Column(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(8.dp)
+        modifier = Modifier.fillMaxSize().padding(8.dp),
     ) {
         Icon(
             imageVector = quality.imageVector,
             contentDescription = stringResource(R.string.signal_quality),
-            tint = quality.color
+            tint = quality.color.invoke(),
         )
         Text(text = "${stringResource(R.string.signal)} ${stringResource(quality.nameRes)}")
     }
@@ -134,35 +128,29 @@ fun LoraSignalIndicator(snr: Float, rssi: Int) {
 
 @Composable
 fun Snr(snr: Float, fontSize: TextUnit = MaterialTheme.typography.labelLarge.fontSize) {
-    val color: Color = if (snr > SNR_GOOD_THRESHOLD) {
-        Quality.GOOD.color
-    } else if (snr > SNR_FAIR_THRESHOLD) {
-        Quality.FAIR.color
-    } else {
-        Quality.BAD.color
-    }
+    val color: Color =
+        if (snr > SNR_GOOD_THRESHOLD) {
+            Quality.GOOD.color.invoke()
+        } else if (snr > SNR_FAIR_THRESHOLD) {
+            Quality.FAIR.color.invoke()
+        } else {
+            Quality.BAD.color.invoke()
+        }
 
-    Text(
-        text = "%s %.2fdB".format(stringResource(id = R.string.snr), snr),
-        color = color,
-        fontSize = fontSize
-    )
+    Text(text = "%s %.2fdB".format(stringResource(id = R.string.snr), snr), color = color, fontSize = fontSize)
 }
 
 @Composable
 fun Rssi(rssi: Int, fontSize: TextUnit = MaterialTheme.typography.labelLarge.fontSize) {
-    val color: Color = if (rssi > RSSI_GOOD_THRESHOLD) {
-        Quality.GOOD.color
-    } else if (rssi > RSSI_FAIR_THRESHOLD) {
-        Quality.FAIR.color
-    } else {
-        Quality.BAD.color
-    }
-    Text(
-        text = "%s %ddBm".format(stringResource(id = R.string.rssi), rssi),
-        color = color,
-        fontSize = fontSize
-    )
+    val color: Color =
+        if (rssi > RSSI_GOOD_THRESHOLD) {
+            Quality.GOOD.color.invoke()
+        } else if (rssi > RSSI_FAIR_THRESHOLD) {
+            Quality.FAIR.color.invoke()
+        } else {
+            Quality.BAD.color.invoke()
+        }
+    Text(text = "%s %ddBm".format(stringResource(id = R.string.rssi), rssi), color = color, fontSize = fontSize)
 }
 
 private fun determineSignalQuality(snr: Float, rssi: Int): Quality = when {
