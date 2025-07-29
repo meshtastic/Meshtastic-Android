eed3e10f7bac5d97177601f484c4953c7151d394
DaneEvans <dane@goneepic.com>
2025-07-29 11:11:14 +1000

    Fix/2530 temp scaling (#2546)
    Signed-off-by: DaneEvans <dane@goneepic.com>

---
+++ app/src/test/java/com/geeksville/mesh/ui/metrics/EnvironmentMetricsTest.kt
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2025 Meshtastic LLC
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.geeksville.mesh.ui.metrics
+
+import com.geeksville.mesh.TelemetryProtos
+import com.geeksville.mesh.copy
+import com.geeksville.mesh.util.UnitConversions.celsiusToFahrenheit
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class EnvironmentMetricsTest {
+
+    @Test
+    fun `temperature and soil temperature are converted to Fahrenheit when isFahrenheit is true`() {
+        val initialTemperatureCelsius = 25.0f
+        val initialSoilTemperatureCelsius = 15.0f
+        val expectedTemperatureFahrenheit = celsiusToFahrenheit(initialTemperatureCelsius)
+        val expectedSoilTemperatureFahrenheit = celsiusToFahrenheit(initialSoilTemperatureCelsius)
+
+        val telemetry =
+            TelemetryProtos.Telemetry.newBuilder()
+                .setEnvironmentMetrics(
+                    TelemetryProtos.EnvironmentMetrics.newBuilder()
+                        .setTemperature(initialTemperatureCelsius)
+                        .setSoilTemperature(initialSoilTemperatureCelsius)
+                        .build(),
+                )
+                .setTime(1000)
+                .build()
+
+        val data = listOf(telemetry)
+
+        val isFahrenheit = true
+
+        val processedTelemetries =
+            if (isFahrenheit) {
+                data.map { tel ->
+                    val temperatureFahrenheit = celsiusToFahrenheit(tel.environmentMetrics.temperature)
+                    val soilTemperatureFahrenheit = celsiusToFahrenheit(tel.environmentMetrics.soilTemperature)
+                    tel.copy {
+                        environmentMetrics =
+                            tel.environmentMetrics.copy {
+                                temperature = temperatureFahrenheit
+                                soilTemperature = soilTemperatureFahrenheit
+                            }
+                    }
+                }
+            } else {
+                data
+            }
+
+        val resultTelemetry = processedTelemetries.first()
+
+        assertEquals(expectedTemperatureFahrenheit, resultTelemetry.environmentMetrics.temperature, 0.01f)
+        assertEquals(expectedSoilTemperatureFahrenheit, resultTelemetry.environmentMetrics.soilTemperature, 0.01f)
+    }
+}
