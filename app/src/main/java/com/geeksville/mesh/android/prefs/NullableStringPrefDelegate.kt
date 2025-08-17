0b8878e147eb8395d27ae5a57f0729015de3e9a3
Phil Oliver <3497406+poliver@users.noreply.github.com>
2025-08-17 17:04:45 -0400

    Add prefs repos and providers

---
+++ app/src/main/java/com/geeksville/mesh/android/prefs/NullableStringPrefDelegate.kt
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2025 Meshtastic LLC
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.geeksville.mesh.android.prefs
+
+import android.content.SharedPreferences
+import androidx.core.content.edit
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
+
+/**
+ * A [ReadWriteProperty] delegate that provides concise, type-safe access to [SharedPreferences] for nullable strings.
+ *
+ * @param prefs The [SharedPreferences] instance to back the property.
+ * @param key The key used to store and retrieve the value.
+ * @param defaultValue The default value to return if no value is found.
+ */
+class NullableStringPrefDelegate(
+    private val prefs: SharedPreferences,
+    private val key: String,
+    private val defaultValue: String?,
+) : ReadWriteProperty<Any?, String?> {
+
+    override fun getValue(thisRef: Any?, property: KProperty<*>): String? = prefs.getString(key, defaultValue)
+
+    override fun setValue(thisRef: Any?, property: KProperty<*>, value: String?) {
+        prefs.edit {
+            when (value) {
+                null -> remove(key)
+                else -> putString(key, value)
+            }
+        }
+    }
+}
