e37bfe41031ddea449728a75fad0d84fe4c91f49
Phil Oliver <3497406+poliver@users.noreply.github.com>
2025-08-20 18:14:26 -0400

    Update network devices tab

--- app/src/main/java/com/geeksville/mesh/ui/connections/components/BLEDevices.kt
+++ app/src/main/java/com/geeksville/mesh/ui/connections/components/BLEDevices.kt
@@ -23,25 +23,20 @@ import android.os.Build
 import android.provider.Settings.ACTION_BLUETOOTH_SETTINGS
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.BluetoothDisabled
 import androidx.compose.material.icons.rounded.BluetoothDisabled
 import androidx.compose.material.icons.rounded.Search
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -49,10 +44,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.geeksville.mesh.R
@@ -171,15 +164,7 @@ fun BLEDevices(
                     } else {
                         Spacer(modifier = Modifier.height(8.dp))
 
-                        Text(
-                            stringResource(R.string.bluetooth_paired_devices),
-                            modifier = Modifier.padding(horizontal = 16.dp).fillMaxWidth(),
-                            style = MaterialTheme.typography.titleLarge,
-                        )
-
-                        Spacer(modifier = Modifier.height(8.dp))
-
-                        Card {
+                        TitledCard(title = stringResource(R.string.bluetooth_paired_devices)) {
                             btDevices.forEach { device ->
                                 DeviceListItem(
                                     connectionState = connectionState,
@@ -213,30 +198,6 @@ fun BLEDevices(
                 },
             )
         }
-    }
-}
-
-@Composable
-private fun EmptyStateContent(
-    imageVector: ImageVector? = null,
-    text: String,
-    actionButton: @Composable (() -> Unit)? = null,
-) {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
-    ) {
-        imageVector?.let { Icon(imageVector = imageVector, contentDescription = text, modifier = Modifier.size(96.dp)) }
-
-        Text(
-            text = text,
-            style = MaterialTheme.typography.bodyLarge,
-            modifier = Modifier.padding(vertical = 8.dp),
-            textAlign = TextAlign.Center,
-        )
-
-        actionButton?.invoke()
     }
 }
 
