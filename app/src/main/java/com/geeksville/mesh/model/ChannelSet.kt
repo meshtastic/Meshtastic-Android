6b42713887571d37e7c06deed3fb674856795daf
Benjamin Faershtein <119711889+RCGV1@users.noreply.github.com>
2025-08-26 20:27:13 -0700

    Added the ability to chose to export a qr with add or replace

--- app/src/main/java/com/geeksville/mesh/model/ChannelSet.kt
+++ app/src/main/java/com/geeksville/mesh/model/ChannelSet.kt
@@ -30,72 +30,61 @@ import kotlin.jvm.Throws
 
 private const val MESHTASTIC_HOST = "meshtastic.org"
 private const val CHANNEL_PATH = "/e/"
-internal const val URL_PREFIX = "https://$MESHTASTIC_HOST$CHANNEL_PATH#"
+internal const val URL_PREFIX = "https://$MESHTASTIC_HOST$CHANNEL_PATH"
 private const val BASE64FLAGS = Base64.URL_SAFE + Base64.NO_WRAP + Base64.NO_PADDING
 
 /**
  * Return a [ChannelSet] that represents the ChannelSet encoded by the URL.
+ *
  * @throws MalformedURLException when not recognized as a valid Meshtastic URL
  */
 @Throws(MalformedURLException::class)
 fun Uri.toChannelSet(): ChannelSet {
-    if (fragment.isNullOrBlank() ||
-        !host.equals(MESHTASTIC_HOST, true) ||
-        !path.equals(CHANNEL_PATH, true)
-    ) {
+    if (fragment.isNullOrBlank() || !host.equals(MESHTASTIC_HOST, true) || !path.equals(CHANNEL_PATH, true)) {
         throw MalformedURLException("Not a valid Meshtastic URL: ${toString().take(40)}")
     }
 
     // Older versions of Meshtastic clients (Apple/web) included `?add=true` within the URL fragment.
     // This gracefully handles those cases until the newer version are generally available/used.
     val url = ChannelSet.parseFrom(Base64.decode(fragment!!.substringBefore('?'), BASE64FLAGS))
-    val shouldAdd = fragment?.substringAfter('?', "")
-        ?.takeUnless { it.isBlank() }
-        ?.equals("add=true")
-        ?: getBooleanQueryParameter("add", false)
+    val shouldAdd =
+        fragment?.substringAfter('?', "")?.takeUnless { it.isBlank() }?.equals("add=true")
+            ?: getBooleanQueryParameter("add", false)
 
     return url.toBuilder().apply { if (shouldAdd) clearLoraConfig() }.build()
 }
 
-/**
- * @return A list of globally unique channel IDs usable with MQTT subscribe()
- */
+/** @return A list of globally unique channel IDs usable with MQTT subscribe() */
 val ChannelSet.subscribeList: List<String>
     get() = settingsList.filter { it.downlinkEnabled }.map { Channel(it, loraConfig).name }
 
 fun ChannelSet.getChannel(index: Int): Channel? =
     if (settingsCount > index) Channel(getSettings(index), loraConfig) else null
 
-/**
- * Return the primary channel info
- */
-val ChannelSet.primaryChannel: Channel? get() = getChannel(0)
+/** Return the primary channel info */
+val ChannelSet.primaryChannel: Channel?
+    get() = getChannel(0)
 
 /**
  * Return a URL that represents the [ChannelSet]
+ *
  * @param upperCasePrefix portions of the URL can be upper case to make for more efficient QR codes
  */
-fun ChannelSet.getChannelUrl(upperCasePrefix: Boolean = false): Uri {
+fun ChannelSet.getChannelUrl(upperCasePrefix: Boolean = false, shouldAdd: Boolean = false): Uri {
     val channelBytes = this.toByteArray() ?: ByteArray(0) // if unset just use empty
     val enc = Base64.encodeToString(channelBytes, BASE64FLAGS)
     val p = if (upperCasePrefix) URL_PREFIX.uppercase() else URL_PREFIX
-    return Uri.parse("$p$enc")
+    val query = if (shouldAdd) "?add=true" else ""
+    return Uri.parse("$p$query#$enc")
 }
 
-val ChannelSet.qrCode: Bitmap?
-    get() = try {
-        val multiFormatWriter = MultiFormatWriter()
-
-        val bitMatrix =
-            multiFormatWriter.encode(
-                getChannelUrl(false).toString(),
-                BarcodeFormat.QR_CODE,
-                960,
-                960
-            )
-        val barcodeEncoder = BarcodeEncoder()
-        barcodeEncoder.createBitmap(bitMatrix)
-    } catch (ex: Throwable) {
-        errormsg("URL was too complex to render as barcode")
-        null
-    }
+fun ChannelSet.qrCode(shouldAdd: Boolean): Bitmap? = try {
+    val multiFormatWriter = MultiFormatWriter()
+    val bitMatrix =
+        multiFormatWriter.encode(getChannelUrl(false, shouldAdd).toString(), BarcodeFormat.QR_CODE, 960, 960)
+    val barcodeEncoder = BarcodeEncoder()
+    barcodeEncoder.createBitmap(bitMatrix)
+} catch (ex: Throwable) {
+    errormsg("URL was too complex to render as barcode")
+    null
+}
