92e0773d75c33ee53ddf2189acaafa6bfe3ea7de
Robert-0410 <62630290+Robert-0410@users.noreply.github.com>
2025-08-30 19:10:55 -0700

    Update SecurityIcon.kt
    Suppress too many functions warning.

--- app/src/main/java/com/geeksville/mesh/ui/common/components/SecurityIcon.kt
+++ app/src/main/java/com/geeksville/mesh/ui/common/components/SecurityIcon.kt
@@ -15,6 +15,8 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+@file:Suppress("TooManyFunctions")
+
 package com.geeksville.mesh.ui.common.components
 
 import androidx.annotation.StringRes
