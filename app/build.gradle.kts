823ed8c60961ab2a95cd0b62d0e543adbb87eae2
James Rich <2199651+jamesarich@users.noreply.github.com>
2025-09-10 11:58:18 -0500

    fix: merge conflicts
    Signed-off-by: James Rich <2199651+jamesarich@users.noreply.github.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,6 +16,7 @@
  */
 
 import io.gitlab.arturbosch.detekt.Detekt
+import org.gradle.kotlin.dsl.invoke
 import java.io.FileInputStream
 import java.util.Properties
 
@@ -60,12 +61,18 @@ android {
         minSdk = Configs.MIN_SDK
         targetSdk = Configs.TARGET_SDK
         // Prioritize injected props, then ENV, then fallback to git commit count
-        versionCode = (project.findProperty("android.injected.version.code")?.toString()?.toInt()
-            ?: System.getenv("VERSION_CODE")?.toInt()
-            ?: gitVersionProvider.get().toInt())
-        versionName = (project.findProperty("android.injected.version.name")?.toString()
-            ?: System.getenv("VERSION_NAME")
-            ?: Configs.VERSION_NAME_BASE)
+        versionCode =
+            (
+                project.findProperty("android.injected.version.code")?.toString()?.toInt()
+                    ?: System.getenv("VERSION_CODE")?.toInt()
+                    ?: gitVersionProvider.get().toInt()
+                )
+        versionName =
+            (
+                project.findProperty("android.injected.version.name")?.toString()
+                    ?: System.getenv("VERSION_NAME")
+                    ?: Configs.VERSION_NAME_BASE
+                )
         testInstrumentationRunner = "com.geeksville.mesh.TestRunner"
         buildConfigField("String", "MIN_FW_VERSION", "\"${Configs.MIN_FW_VERSION}\"")
         buildConfigField("String", "ABS_MIN_FW_VERSION", "\"${Configs.ABS_MIN_FW_VERSION}\"")
