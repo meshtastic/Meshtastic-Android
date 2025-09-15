ebee354d1677cce572b2741d7bbedd47152e89cc
James Rich <2199651+jamesarich@users.noreply.github.com>
2025-09-15 15:15:13 -0500

    feat(build-logic): Enable Compose compiler metrics and reports
    This commit enables the generation of Compose compiler metrics and reports.
    The output directories for these reports are configured to be relative to the root project's build directory.
    Additionally, a stability configuration file `compose_compiler_config.conf` is added.
    
    Signed-off-by: James Rich <2199651+jamesarich@users.noreply.github.com>

--- build-logic/convention/src/main/kotlin/com/geeksville/mesh/buildlogic/AndroidCompose.kt
+++ build-logic/convention/src/main/kotlin/com/geeksville/mesh/buildlogic/AndroidCompose.kt
@@ -18,7 +18,10 @@ package com.geeksville.mesh.buildlogic
 
 import com.android.build.api.dsl.CommonExtension
 import org.gradle.api.Project
+import org.gradle.api.provider.Provider
+import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
+import org.jetbrains.kotlin.compose.compiler.gradle.ComposeCompilerGradlePluginExtension
 
 /**
  * Configure Compose-specific options
@@ -50,23 +53,23 @@ internal fun Project.configureAndroidCompose(
         }
     }
 
-//    extensions.configure<ComposeCompilerGradlePluginExtension> {
-//        fun Provider<String>.onlyIfTrue() = flatMap { provider { it.takeIf(String::toBoolean) } }
-//        fun Provider<*>.relativeToRootProject(dir: String) = map {
-//            isolated.rootProject.projectDirectory
-//                .dir("build")
-//                .dir(projectDir.toRelativeString(rootDir))
-//        }.map { it.dir(dir) }
-//
-//        project.providers.gradleProperty("enableComposeCompilerMetrics").onlyIfTrue()
-//            .relativeToRootProject("compose-metrics")
-//            .let(metricsDestination::set)
-//
-//        project.providers.gradleProperty("enableComposeCompilerReports").onlyIfTrue()
-//            .relativeToRootProject("compose-reports")
-//            .let(reportsDestination::set)
-//
-//        stabilityConfigurationFiles
-//            .add(isolated.rootProject.projectDirectory.file("compose_compiler_config.conf"))
-//    }
+    extensions.configure<ComposeCompilerGradlePluginExtension> {
+        fun Provider<String>.onlyIfTrue() = flatMap { provider { it.takeIf(String::toBoolean) } }
+        fun Provider<*>.relativeToRootProject(dir: String) = map {
+            isolated.rootProject.projectDirectory
+                .dir("build")
+                .dir(projectDir.toRelativeString(rootDir))
+        }.map { it.dir(dir) }
+
+        project.providers.gradleProperty("enableComposeCompilerMetrics").onlyIfTrue()
+            .relativeToRootProject("compose-metrics")
+            .let(metricsDestination::set)
+
+        project.providers.gradleProperty("enableComposeCompilerReports").onlyIfTrue()
+            .relativeToRootProject("compose-reports")
+            .let(reportsDestination::set)
+
+        stabilityConfigurationFiles
+            .add(isolated.rootProject.projectDirectory.file("compose_compiler_config.conf"))
+    }
 }
