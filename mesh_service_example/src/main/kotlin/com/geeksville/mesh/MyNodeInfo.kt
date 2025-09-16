423eb513495303709446ce23501d1f6fc764649c
Phil Oliver <3497406+poliver@users.noreply.github.com>
2025-09-16 19:17:07 -0400

    Run spotless

--- mesh_service_example/src/main/kotlin/com/geeksville/mesh/MyNodeInfo.kt
+++ mesh_service_example/src/main/kotlin/com/geeksville/mesh/MyNodeInfo.kt
@@ -23,21 +23,22 @@ import kotlinx.parcelize.Parcelize
 // MyNodeInfo sent via special protobuf from radio
 @Parcelize
 data class MyNodeInfo(
-        val myNodeNum: Int,
-        val hasGPS: Boolean,
-        val model: String?,
-        val firmwareVersion: String?,
-        val couldUpdate: Boolean, // this application contains a software load we _could_ install if you want
-        val shouldUpdate: Boolean, // this device has old firmware
-        val currentPacketId: Long,
-        val messageTimeoutMsec: Int,
-        val minAppVersion: Int,
-        val maxChannels: Int,
-        val hasWifi: Boolean,
-        val channelUtilization: Float,
-        val airUtilTx: Float,
-        val deviceId: String?,
+    val myNodeNum: Int,
+    val hasGPS: Boolean,
+    val model: String?,
+    val firmwareVersion: String?,
+    val couldUpdate: Boolean, // this application contains a software load we _could_ install if you want
+    val shouldUpdate: Boolean, // this device has old firmware
+    val currentPacketId: Long,
+    val messageTimeoutMsec: Int,
+    val minAppVersion: Int,
+    val maxChannels: Int,
+    val hasWifi: Boolean,
+    val channelUtilization: Float,
+    val airUtilTx: Float,
+    val deviceId: String?,
 ) : Parcelable {
-        /** A human readable description of the software/hardware version */
-        val firmwareString: String get() = "$model $firmwareVersion"
+    /** A human readable description of the software/hardware version */
+    val firmwareString: String
+        get() = "$model $firmwareVersion"
 }
