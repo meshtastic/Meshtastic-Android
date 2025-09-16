57efcbc4f39830e2c1c4929c43b310ce837b72f9
Phil Oliver <3497406+poliver@users.noreply.github.com>
2025-09-16 13:10:18 -0400

    Update DI module providers

--- network/src/google/java/com/geeksville/mesh/network/di/ApiModule.kt
+++ network/src/google/java/com/geeksville/mesh/network/di/ApiModule.kt
@@ -30,87 +30,76 @@ import com.datadog.android.okhttp.DatadogEventListener
 import com.datadog.android.okhttp.DatadogInterceptor
 import com.geeksville.mesh.network.BuildConfig
 import com.geeksville.mesh.network.retrofit.ApiService
+import com.geeksville.mesh.network.retrofit.createApiService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import de.jensklingenberg.ktorfit.Ktorfit
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.okhttp.OkHttp
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
-import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Retrofit
-import retrofit2.converter.kotlinx.serialization.asConverterFactory
 import javax.inject.Singleton
 
 private const val DISK_CACHE_PERCENT = 0.02
 private const val MEMORY_CACHE_PERCENT = 0.25
+
 @InstallIn(SingletonComponent::class)
 @Module
 class ApiModule {
     @Provides
     @Singleton
-    fun provideOkHttpClient(): OkHttpClient {
-
-        val loggingInterceptor = HttpLoggingInterceptor().apply {
-            if (BuildConfig.DEBUG) {
-                setLevel(HttpLoggingInterceptor.Level.BODY)
+    fun provideHttpClient(): HttpClient = HttpClient(engineFactory = OkHttp) {
+        engine {
+            config {
+                addInterceptor(
+                    interceptor =
+                    HttpLoggingInterceptor().apply {
+                        if (BuildConfig.DEBUG) {
+                            setLevel(HttpLoggingInterceptor.Level.BODY)
+                        }
+                    },
+                )
+                addInterceptor(
+                    interceptor = DatadogInterceptor.Builder(tracedHosts = listOf("meshtastic.org")).build(),
+                )
+                eventListenerFactory(eventListenerFactory = DatadogEventListener.Factory())
             }
         }
-        val tracedHosts = listOf("meshtastic.org")
-        return OkHttpClient.Builder()
-            .addInterceptor(loggingInterceptor)
-            .addInterceptor(DatadogInterceptor.Builder(tracedHosts).build())
-            .eventListenerFactory(DatadogEventListener.Factory())
-            .build()
-    }
 
-    @Provides
-    @Singleton
-    fun provideRetrofit(
-        okHttpClient: OkHttpClient
-    ): Retrofit {
-        return Retrofit.Builder()
-            .baseUrl("https://api.meshtastic.org/") // Replace with your base URL
-            .addConverterFactory(
-                Json.asConverterFactory(
-                    "application/json; charset=UTF8".toMediaType()
-                )
+        install(ContentNegotiation) {
+            json(
+                Json {
+                    isLenient = true
+                    ignoreUnknownKeys = true
+                },
             )
-            .client(okHttpClient)
-            .build()
+        }
     }
 
     @Provides
     @Singleton
-    fun provideApiService(retrofit: Retrofit): ApiService {
-        return retrofit.create(ApiService::class.java)
+    fun provideApiService(httpClient: HttpClient): ApiService {
+        val ktorfit = Ktorfit.Builder().baseUrl("https://api.meshtastic.org/").httpClient(httpClient).build()
+        return ktorfit.createApiService()
     }
 
     @Provides
     @Singleton
-    fun imageLoader(
-        httpClient: OkHttpClient,
-        @ApplicationContext application: Context,
-    ): ImageLoader {
+    fun imageLoader(httpClient: OkHttpClient, @ApplicationContext application: Context): ImageLoader {
         val sharedOkHttp = httpClient.newBuilder().build()
         return ImageLoader.Builder(application)
             .components {
-                add(
-                    OkHttpNetworkFetcherFactory({ sharedOkHttp })
-                )
+                add(OkHttpNetworkFetcherFactory({ sharedOkHttp }))
                 add(SvgDecoder.Factory())
             }
-            .memoryCache {
-                MemoryCache.Builder()
-                    .maxSizePercent(application, MEMORY_CACHE_PERCENT)
-                    .build()
-            }
-            .diskCache {
-                DiskCache.Builder()
-                    .maxSizePercent(DISK_CACHE_PERCENT)
-                    .build()
-            }
+            .memoryCache { MemoryCache.Builder().maxSizePercent(application, MEMORY_CACHE_PERCENT).build() }
+            .diskCache { DiskCache.Builder().maxSizePercent(DISK_CACHE_PERCENT).build() }
             .logger(if (BuildConfig.DEBUG) DebugLogger(Logger.Level.Verbose) else null)
             .crossfade(true)
             .build()
