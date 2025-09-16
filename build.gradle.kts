77682938681702c844843afe396737b04265d679
Phil Oliver <3497406+poliver@users.noreply.github.com>
2025-09-16 13:05:55 -0400

    Add ktor/ktorfit dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,7 @@ plugins {
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.parcelize) apply false
     alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.ktorfit) apply false
     alias(libs.plugins.protobuf) apply false
     alias(libs.plugins.secrets) apply false
     alias(libs.plugins.dependency.analysis)
