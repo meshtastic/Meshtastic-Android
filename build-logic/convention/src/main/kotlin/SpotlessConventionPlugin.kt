e4bfce0989c699ac687620148f0664dc1f771a93
James Rich <2199651+jamesarich@users.noreply.github.com>
2025-09-17 16:43:23 -0500

    refactor(build): simplify lint, spotless, and detekt configurations (#3133)
    Signed-off-by: James Rich <2199651+jamesarich@users.noreply.github.com>

--- build-logic/convention/src/main/kotlin/SpotlessConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/SpotlessConventionPlugin.kt
@@ -1,4 +1,5 @@
 import com.diffplug.gradle.spotless.SpotlessExtension
+import com.geeksville.mesh.buildlogic.configureKotlinJvm
 import com.geeksville.mesh.buildlogic.configureSpotless
 import com.geeksville.mesh.buildlogic.libs
 import org.gradle.api.Plugin
@@ -9,6 +10,7 @@ import org.gradle.kotlin.dsl.getByType
 class SpotlessConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
+            configureKotlinJvm()
             apply(plugin = libs.findPlugin("spotless").get().get().pluginId)
             val extension = extensions.getByType<SpotlessExtension>()
             configureSpotless(extension)
