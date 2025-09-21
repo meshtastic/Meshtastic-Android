5cc820de6d3f356be4c68b3169920896d4ef2a4c
James Rich <2199651+jamesarich@users.noreply.github.com>
2025-09-21 16:25:05 -0500

    spotless
    Signed-off-by: James Rich <2199651+jamesarich@users.noreply.github.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,6 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-import com.datadog.gradle.plugin.InstrumentationMode
 import com.geeksville.mesh.buildlogic.Configs
 import com.geeksville.mesh.buildlogic.GitVersionValueSource
 import java.io.FileInputStream
