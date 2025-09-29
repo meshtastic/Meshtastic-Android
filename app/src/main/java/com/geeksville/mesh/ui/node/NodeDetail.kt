83e5e7600ba4ad59a49c8745e1a3b40a7dcf31a7
James Rich <2199651+jamesarich@users.noreply.github.com>
2025-09-29 15:47:59 -0500

    fix copyright
    Signed-off-by: James Rich <2199651+jamesarich@users.noreply.github.com>

--- app/src/main/java/com/geeksville/mesh/ui/node/NodeDetail.kt
+++ app/src/main/java/com/geeksville/mesh/ui/node/NodeDetail.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024-2025 Meshtastic LLC
+ * Copyright (c) 2025 Meshtastic LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
