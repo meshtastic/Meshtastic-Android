191b6bdda9150131f3ce5405505ac5a78f931a94
Phil Oliver <3497406+poliver@users.noreply.github.com>
2025-09-30 15:14:47 -0400

    Remove duplicate files

--- mesh_service_example/src/main/kotlin/com/meshtastic/android/meshserviceexample/MainActivity.kt
+++ mesh_service_example/src/main/kotlin/com/meshtastic/android/meshserviceexample/MainActivity.kt
@@ -36,10 +36,10 @@ import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.geeksville.mesh.NodeInfo
 import com.geeksville.mesh.Portnums
 import org.meshtastic.core.model.DataPacket
 import org.meshtastic.core.model.MessageStatus
+import org.meshtastic.core.model.NodeInfo
 import org.meshtastic.core.service.IMeshService
 
 private const val TAG: String = "MeshServiceExample"
