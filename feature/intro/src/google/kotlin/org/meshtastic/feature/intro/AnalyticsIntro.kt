ed78cc7f8faa7ad46f76b8d8452db66391f0c123
James Rich <2199651+jamesarich@users.noreply.github.com>
2025-10-08 19:53:06 -0500

    feat: Externalize analytics strings
    Moves hardcoded strings from the AnalyticsIntro composable to `strings.xml`. This improves localization and maintainability.
    
    Signed-off-by: James Rich <2199651+jamesarich@users.noreply.github.com>

--- feature/intro/src/google/kotlin/org/meshtastic/feature/intro/AnalyticsIntro.kt
+++ feature/intro/src/google/kotlin/org/meshtastic/feature/intro/AnalyticsIntro.kt
@@ -24,9 +24,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import org.meshtastic.core.strings.R
 import org.meshtastic.core.ui.component.AutoLinkText
 
 @Composable
@@ -37,22 +39,17 @@ fun AnalyticsIntro(modifier: Modifier = Modifier) {
         verticalArrangement = Arrangement.spacedBy(8.dp),
     ) {
         val textModifier = Modifier.fillMaxWidth().align(Alignment.CenterHorizontally)
-        Text(
-            modifier = textModifier,
-            textAlign = TextAlign.Center,
-            text =
-            "Analytics are collected to help us improve the Android app (thank you), we will receive anonymized information about user behavior. This includes crash reports, screens used in the app, etc.",
-        )
-        Text(modifier = textModifier, textAlign = TextAlign.Center, text = "Analytics platforms used are: ")
-        AutoLinkText("Firebase https://firebase.google.com/")
-        AutoLinkText("Datadog  https://www.datadoghq.com/")
+        Text(modifier = textModifier, textAlign = TextAlign.Center, text = stringResource(R.string.analytics_notice))
+        Text(modifier = textModifier, textAlign = TextAlign.Center, text = stringResource(R.string.analytics_platforms))
+        AutoLinkText(stringResource(R.string.firebase_link))
+        AutoLinkText(stringResource(R.string.datadog_link))
 
         Text(
             modifier = textModifier,
             textAlign = TextAlign.Center,
-            text = "For more information, see our privacy policy.",
+            text = stringResource(R.string.for_more_information_see_our_privacy_policy),
         )
-        AutoLinkText(text = " https://meshtastic.org/docs/legal/privacy/")
+        AutoLinkText(text = stringResource(R.string.privacy_url))
     }
 }
 
