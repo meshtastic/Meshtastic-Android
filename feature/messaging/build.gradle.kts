4b34710e07c58c21de11f98245d92ee5ddbad06e
Phil Oliver <3497406+poliver@users.noreply.github.com>
2025-10-10 12:03:19 -0400

    Add :feature:messaging

---
+++ feature/messaging/build.gradle.kts
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2025 Meshtastic LLC
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+plugins {
+    alias(libs.plugins.kover)
+    alias(libs.plugins.meshtastic.android.library)
+    alias(libs.plugins.meshtastic.kotlinx.serialization)
+}
+
+android { namespace = "org.meshtastic.feature.messaging" }
+
+dependencies {
+    implementation(projects.core.strings)
+    googleImplementation(projects.core.ui)
+
+    implementation(libs.accompanist.permissions)
+    implementation(libs.androidx.compose.material.iconsExtended)
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.ui.text)
+    implementation(libs.androidx.navigation3.runtime)
+    implementation(libs.androidx.navigation3.ui)
+}
