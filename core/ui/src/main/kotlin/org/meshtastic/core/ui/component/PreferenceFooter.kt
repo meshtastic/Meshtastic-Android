273772b214f126520433ed31a13727a6fb9d7feb
Phil Oliver <3497406+poliver@users.noreply.github.com>
2025-10-13 17:40:55 -0400

    Tweak settings footer button style/visibility (#3458)

--- core/ui/src/main/kotlin/org/meshtastic/core/ui/component/PreferenceFooter.kt
+++ core/ui/src/main/kotlin/org/meshtastic/core/ui/component/PreferenceFooter.kt
@@ -23,8 +23,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -63,14 +63,22 @@ fun PreferenceFooter(
     modifier: Modifier = Modifier,
 ) {
     Row(
-        modifier = modifier.fillMaxWidth().padding(16.dp),
+        modifier = modifier.fillMaxWidth().padding(horizontal = 24.dp, vertical = 16.dp),
         horizontalArrangement = Arrangement.spacedBy(8.dp),
         verticalAlignment = Alignment.CenterVertically,
     ) {
-        OutlinedButton(modifier = Modifier.height(48.dp).weight(1f), onClick = onNegativeClicked) {
+        ElevatedButton(
+            modifier = Modifier.height(48.dp).weight(1f),
+            colors = ButtonDefaults.filledTonalButtonColors(),
+            onClick = onNegativeClicked,
+        ) {
             Text(text = negativeText)
         }
-        Button(modifier = Modifier.height(48.dp).weight(1f), enabled = enabled, onClick = onPositiveClicked) {
+        ElevatedButton(
+            modifier = Modifier.height(48.dp).weight(1f),
+            colors = ButtonDefaults.buttonColors(),
+            onClick = { if (enabled) onPositiveClicked() },
+        ) {
             Text(text = positiveText)
         }
     }
