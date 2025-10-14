dd7b66fd7d3dcb0ad5bfd466ab94fb5379549333
Phil Oliver <3497406+poliver@users.noreply.github.com>
2025-10-13 22:25:48 -0400

    Revert

--- core/ui/src/main/kotlin/org/meshtastic/core/ui/component/SettingsItem.kt
+++ core/ui/src/main/kotlin/org/meshtastic/core/ui/component/SettingsItem.kt
@@ -19,9 +19,7 @@ package org.meshtastic.core.ui.component
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.rounded.KeyboardArrowRight
 import androidx.compose.material.icons.rounded.Android
@@ -31,7 +29,6 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.LocalContentColor
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -43,32 +40,67 @@ import androidx.compose.ui.unit.dp
 import org.meshtastic.core.ui.theme.AppTheme
 
 /**
- * A clickable settings item with optional supporting text and trailing content. Defaults to a trailing arrow icon if no
- * custom trailing content is provided.
+ * A settings item with an optional [leadingIcon], headline [text], optional [supportingText], and optional
+ * [trailingIcon].
  */
 @Composable
 fun SettingsItem(
     text: String,
     supportingText: String? = null,
     textColor: Color = LocalContentColor.current,
+    supportingTextColor: Color = LocalContentColor.current,
+    enabled: Boolean = true,
+    leadingIcon: ImageVector? = null,
+    leadingIconTint: Color = LocalContentColor.current,
+    trailingIcon: ImageVector? = Icons.AutoMirrored.Rounded.KeyboardArrowRight,
+    trailingIconTint: Color = LocalContentColor.current,
+    onClick: (() -> Unit)? = null,
+) {
+    BasicSettingsItem(
+        text = text,
+        textColor = textColor,
+        supportingText = supportingText,
+        supportingTextColor = supportingTextColor,
+        enabled = enabled,
+        leadingIcon = leadingIcon,
+        leadingIconTint = leadingIconTint,
+        trailingContent = { trailingIcon.Icon(trailingIconTint) },
+        onClick = onClick,
+    )
+}
+
+/**
+ * The foundational settings item. It supports a [leadingIcon] (optional), headline [text] and [supportingText]
+ * (optional), and a [trailingContent] composable (optional). The majority of settings items will leverage this, and if
+ * they can't, they should build on [Content].
+ */
+@Composable
+fun BasicSettingsItem(
+    text: String,
+    textColor: Color = LocalContentColor.current,
+    supportingText: String? = null,
+    supportingTextColor: Color = LocalContentColor.current,
     enabled: Boolean = true,
     leadingIcon: ImageVector? = null,
     leadingIconTint: Color = LocalContentColor.current,
     trailingContent: @Composable (() -> Unit)? = null,
-    onClick: () -> Unit,
+    onClick: (() -> Unit)? = null,
 ) {
-    val finalTrailingContent: @Composable (() -> Unit) =
-        trailingContent ?: { Icons.AutoMirrored.Rounded.KeyboardArrowRight.Icon(LocalContentColor.current) }
+    val content: @Composable ColumnScope.() -> Unit = {
+        Content(
+            leading = { leadingIcon.Icon(leadingIconTint) },
+            headline = { Text(text = text, color = textColor) },
+            supporting = supportingText?.let { { Text(text = it, color = supportingTextColor) } },
+            trailing = trailingContent,
+        )
+    }
 
-    SettingsListItem(
-        text = text,
-        textColor = textColor,
-        enabled = enabled,
-        onClick = onClick,
-        leadingContent = { leadingIcon.Icon(leadingIconTint) },
-        supportingContent = { supportingText?.let { Text(text = it, style = MaterialTheme.typography.titleMedium) } },
-        trailingContent = finalTrailingContent,
-    )
+    // Ensures that click ripples are disabled for non-clickable list items
+    if (onClick != null) {
+        ClickableWrapper(enabled = enabled, onClick = onClick, content = content)
+    } else {
+        Column(content = content)
+    }
 }
 
 /** A toggleable settings switch item. */
@@ -82,104 +114,51 @@ fun SettingsItemSwitch(
     leadingIconTint: Color = LocalContentColor.current,
     onClick: () -> Unit,
 ) {
-    SettingsListItem(
+    BasicSettingsItem(
         text = text,
         textColor = textColor,
         enabled = enabled,
-        onClick = onClick,
-        leadingContent = { leadingIcon.Icon(leadingIconTint) },
+        leadingIcon = leadingIcon,
+        leadingIconTint = leadingIconTint,
         trailingContent = { Switch(checked = checked, enabled = enabled, onCheckedChange = null) },
-    )
-}
-
-/** A settings detail item. */
-@Composable
-fun SettingsItemDetail(
-    text: String,
-    supportingText: String?,
-    textColor: Color = LocalContentColor.current,
-    icon: ImageVector? = null,
-    iconTint: Color = LocalContentColor.current,
-    enabled: Boolean = true,
-    onClick: (() -> Unit)? = null,
-) {
-    SettingsListItem(
-        text = text,
-        textColor = textColor,
-        enabled = enabled,
         onClick = onClick,
-        leadingContent = { icon.Icon(iconTint) },
-        supportingContent = {
-            supportingText?.let {
-                Text(
-                    text = it,
-                    style = MaterialTheme.typography.titleLarge,
-                    color = textColor, // Detail style explicitly sets color
-                )
-            }
-        },
-        trailingContent = {},
-    )
-}
-
-/** A settings detail item with composable content for the detail. */
-@Composable
-fun SettingsItemDetail(
-    text: String,
-    textColor: Color = LocalContentColor.current,
-    icon: ImageVector? = null,
-    iconTint: Color = LocalContentColor.current,
-    enabled: Boolean = true,
-    onClick: (() -> Unit)? = null,
-    supportingContent: @Composable () -> Unit,
-) {
-    SettingsListItem(
-        text = text,
-        textColor = textColor,
-        enabled = enabled,
-        onClick = onClick,
-        leadingContent = { icon.Icon(iconTint) },
-        supportingContent = supportingContent,
-        trailingContent = {},
     )
 }
 
 /**
- * Base composable for all settings screen list items. It handles the Material3 [ListItem] structure and the conditional
- * click wrapper.
+ * A clickable Card wrapper used for all clickable settings items. This has a dedicated implementation so that it can be
+ * used by multiple components. This allows its underlying implementation to be changed in a single place, while being
+ * reflected in all components that use it..
  */
 @Composable
-private fun SettingsListItem(
-    text: String,
-    textColor: Color,
-    enabled: Boolean,
-    onClick: (() -> Unit)?,
-    leadingContent: @Composable (() -> Unit)? = null,
-    supportingContent: @Composable (() -> Unit)? = null,
-    trailingContent: @Composable (() -> Unit)? = null,
-) {
-    val listItemContent: @Composable ColumnScope.() -> Unit = {
-        ListItem(
-            modifier = Modifier.padding(horizontal = 8.dp),
-            colors = ListItemDefaults.colors(containerColor = Color.Transparent),
-            headlineContent = { Text(text = text, color = textColor) },
-            supportingContent = { SelectionContainer { supportingContent?.invoke() } },
-            leadingContent = leadingContent,
-            trailingContent = trailingContent,
-        )
-    }
+private fun ClickableWrapper(enabled: Boolean, onClick: () -> Unit, content: @Composable ColumnScope.() -> Unit) {
+    Card(
+        onClick = onClick,
+        enabled = enabled,
+        colors =
+        CardDefaults.cardColors(containerColor = Color.Transparent, disabledContainerColor = Color.Transparent),
+        content = content,
+    )
+}
 
-    if (onClick != null) {
-        Card(
-            onClick = onClick,
-            enabled = enabled,
-            colors =
-            CardDefaults.cardColors(containerColor = Color.Transparent, disabledContainerColor = Color.Transparent),
-            content = listItemContent,
-        )
-    } else {
-        Column(content = listItemContent)
-    }
+/**
+ * The row content to display for a settings item. This is a useful building block for new items that may not align with
+ * [BasicSettingsItem]
+ */
+@Composable
+private fun Content(
+    leading: @Composable () -> Unit,
+    headline: @Composable () -> Unit,
+    supporting: @Composable (() -> Unit)? = null,
+    trailing: @Composable (() -> Unit)? = null,
+) {
+    ListItem(
+        colors = ListItemDefaults.colors(containerColor = Color.Transparent),
+        headlineContent = headline,
+        supportingContent = supporting,
+        leadingContent = leading,
+        trailingContent = trailing,
+    )
 }
 
 @Composable
@@ -207,5 +186,5 @@ private fun SettingsItemSwitchPreview() {
 @Preview(showBackground = true)
 @Composable
 private fun SettingsItemDetailPreview() {
-    AppTheme { SettingsItemDetail(text = "Text 1", icon = Icons.Rounded.Android, supportingText = "Text2") }
+    AppTheme { SettingsItem(text = "Text 1", leadingIcon = Icons.Rounded.Android, supportingText = "Text2") }
 }
