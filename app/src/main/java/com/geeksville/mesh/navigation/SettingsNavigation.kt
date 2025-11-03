1a382d99788d68beed045d845471a68d47531735
Phil Oliver <3497406+poliver@users.noreply.github.com>
2025-11-03 16:52:01 -0500

    Detekt

--- app/src/main/java/com/geeksville/mesh/navigation/SettingsNavigation.kt
+++ app/src/main/java/com/geeksville/mesh/navigation/SettingsNavigation.kt
@@ -15,6 +15,8 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+@file:Suppress("Wrapping", "SpacingAroundColon")
+
 package com.geeksville.mesh.navigation
 
 import androidx.compose.runtime.Composable
