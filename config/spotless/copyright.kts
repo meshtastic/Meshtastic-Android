6950c8f01b1c1e1bf9c29513f99f09704a969b8e
James Rich <2199651+jamesarich@users.noreply.github.com>
2026-01-02 14:06:05 -0600

    Fix: Use dynamic year for copyright headers (#4116)

--- config/spotless/copyright.kts
+++ config/spotless/copyright.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025 Meshtastic LLC
+ * Copyright (c) $YEAR Meshtastic LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
