c03178406abfad196f9e00b4fc7d01481ae0e137
James Rich <2199651+jamesarich@users.noreply.github.com>
2026-01-02 15:51:27 -0600

    fix: Update copyright year to 2026 (#4121)

--- feature/map/build.gradle.kts
+++ feature/map/build.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025 Meshtastic LLC
+ * Copyright (c) 2025-2026 Meshtastic LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
