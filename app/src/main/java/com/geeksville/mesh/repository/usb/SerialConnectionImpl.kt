191373993bc46f62ed51412d41865980b71bb28f
James Rich <2199651+jamesarich@users.noreply.github.com>
2026-01-26 06:30:33 -0600

    spotless
    Signed-off-by: James Rich <2199651+jamesarich@users.noreply.github.com>

--- app/src/main/java/com/geeksville/mesh/repository/usb/SerialConnectionImpl.kt
+++ app/src/main/java/com/geeksville/mesh/repository/usb/SerialConnectionImpl.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025 Meshtastic LLC
+ * Copyright (c) 2025-2026 Meshtastic LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -14,7 +14,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-
 package com.geeksville.mesh.repository.usb
 
 import android.hardware.usb.UsbManager
@@ -39,6 +38,7 @@ internal class SerialConnectionImpl(
     private val closed = AtomicBoolean(false)
     private val ioRef = AtomicReference<SerialInputOutputManager>()
 
+    @Suppress("TooGenericExceptionCaught")
     override fun sendBytes(bytes: ByteArray) {
         ioRef.get()?.let {
             Logger.d { "writing ${bytes.size} byte(s }" }
