44b0270ef8d765977941b7ce43fe1632684c2c62
James Rich <2199651+jamesarich@users.noreply.github.com>
2026-02-05 22:00:17 -0600

    feat(debugging): Make debug filter/search Composables public
    The visibility of several Composables in the debugging feature has been changed from `internal` to `public`. This allows them to be used in UI tests and previews.
    
    The affected Composables are:
    - `DebugPresetFilters`
    - `DebugFilterBar`
    - `DebugActiveFilters`
    - `DebugSearchNavigation`
    - `DebugSearchBar`
    - `DebugSearchState`
    
    Signed-off-by: James Rich <2199651+jamesarich@users.noreply.github.com>

--- core/ui/src/main/kotlin/org/meshtastic/core/ui/timezone/ZoneIdExtensions.kt
+++ core/ui/src/main/kotlin/org/meshtastic/core/ui/timezone/ZoneIdExtensions.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025 Meshtastic LLC
+ * Copyright (c) 2025-2026 Meshtastic LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -14,7 +14,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-
 @file:Suppress("Wrapping", "UnusedImports", "SpacingAroundColon")
 
 package org.meshtastic.core.ui.timezone
