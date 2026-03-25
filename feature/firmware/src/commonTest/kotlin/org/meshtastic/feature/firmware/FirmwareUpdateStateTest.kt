341d8c71b9c37de0a0bdb61dcf51e62b018cdbe4
James Rich <2199651+jamesarich@users.noreply.github.com>
2026-03-25 16:26:49 -0500

    test(firmware): add basic state test for brief coverage pass

---
+++ feature/firmware/src/commonTest/kotlin/org/meshtastic/feature/firmware/FirmwareUpdateStateTest.kt
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2026 Meshtastic LLC
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package org.meshtastic.feature.firmware
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class FirmwareUpdateStateTest {
+
+    @Test
+    fun `ProgressState defaults are correct`() {
+        val state = ProgressState()
+        assertEquals("", state.message)
+        assertEquals(0f, state.progress)
+        assertEquals(null, state.details)
+    }
+
+    @Test
+    fun `ProgressState can be instantiated with values`() {
+        val state = ProgressState("Downloading", 0.5f, "1MB/s")
+        assertEquals("Downloading", state.message)
+        assertEquals(0.5f, state.progress)
+        assertEquals("1MB/s", state.details)
+    }
+}
